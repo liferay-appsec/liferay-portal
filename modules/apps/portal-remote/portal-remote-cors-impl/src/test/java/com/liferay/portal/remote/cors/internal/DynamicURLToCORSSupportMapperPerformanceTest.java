83cddaacd89700e38fceec3d10de3b0eb9d9e952
arthurchan35 <arthurchan35@gmail.com>
2020-08-17 17:47:22 +0800

    DO NOT MERGE - Add performance test for dynamic impl

---
+++ modules/apps/portal-remote/portal-remote-cors-impl/src/test/java/com/liferay/portal/remote/cors/internal/DynamicURLToCORSSupportMapperPerformanceTest.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.remote.cors.internal;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Arthur Chan
+ */
+public class DynamicURLToCORSSupportMapperPerformanceTest
+	extends BaseURLToCORSSupportMapperTestBase {
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		setLogFilePath("dynamic-url-to-cors-support-mapper-performance.log");
+	}
+
+	@Before
+	public void setUp() {
+		_dynamicURLToCORSSupportMapper = new DynamicURLToCORSSupportMapper(
+			super.buildCORSSupports());
+	}
+
+	@Test
+	public void testGet() throws Exception {
+		super.testGetPerformance(_dynamicURLToCORSSupportMapper);
+	}
+
+	private URLToCORSSupportMapper _dynamicURLToCORSSupportMapper;
+
+}
