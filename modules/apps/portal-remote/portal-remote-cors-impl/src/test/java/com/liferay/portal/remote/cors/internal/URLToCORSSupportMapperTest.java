945c8e7f607dae499e4fc7a7c6c6e96120cdb1f8
arthurchan35 <arthurchan35@gmail.com>
2020-08-17 13:09:11 +0800

    LPS-119169 Rewrite unit tests using base test

--- modules/apps/portal-remote/portal-remote-cors-impl/src/test/java/com/liferay/portal/remote/cors/internal/URLToCORSSupportMapperTest.java
+++ modules/apps/portal-remote/portal-remote-cors-impl/src/test/java/com/liferay/portal/remote/cors/internal/URLToCORSSupportMapperTest.java
@@ -14,110 +14,26 @@
 
 package com.liferay.portal.remote.cors.internal;
 
-import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.util.KeyValuePair;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.junit.Assert;
-import org.junit.ComparisonFailure;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * @author Arthur Chan
  */
-public class URLToCORSSupportMapperTest {
+public class URLToCORSSupportMapperTest
+	extends BaseURLToCORSSupportMapperTestBase {
+
+	@Before
+	public void setUp() {
+		_urlToCORSSupportMapper = new URLToCORSSupportMapper(
+			buildCORSSupports());
+	}
 
 	@Test
 	public void testGet() throws Exception {
-		Map<String, CORSSupport> corsSupports = new HashMap<>();
-
-		KeyValuePair[] keyValuePairs = {
-			new KeyValuePair("/", "//*"), new KeyValuePair("/*", "/*/*"),
-			new KeyValuePair("/*/", "/*//*"),
-			new KeyValuePair("/c/portal/j_login", "/c/portal/j_login"),
-			new KeyValuePair("/documents", "/documents/*"),
-			new KeyValuePair("/documents/", "/documents/*"),
-			new KeyValuePair("/documents/main.jsp", "/documents/main.jsp/*"),
-			new KeyValuePair("/documents/main.jsp/*", "/documents/main.jsp/*"),
-			new KeyValuePair("/documents/main.jspf", "/documents/main.jspf/*"),
-			new KeyValuePair("/documents/main.jspf/", "/documents/main.jspf/*"),
-			new KeyValuePair("/documents/user1", "/documents/user1/*"),
-			new KeyValuePair("/documents/user1/", "/documents/user1/*"),
-			new KeyValuePair(
-				"/documents/user1/folder1", "/documents/user1/folder1/*"),
-			new KeyValuePair(
-				"/documents/user1/folder1/", "/documents/user1/folder1/*"),
-			new KeyValuePair(
-				"/documents/user1/folder2", "/documents/user1/folder2/*"),
-			new KeyValuePair(
-				"/documents/user1/folder2/", "/documents/user1/folder2/*"),
-			new KeyValuePair("/documents/user2", "/documents/user2/*"),
-			new KeyValuePair("/documents/user2/", "/documents/user2/*"),
-			new KeyValuePair(
-				"/documents/user2/folder1", "/documents/user2/folder1/*"),
-			new KeyValuePair(
-				"/documents/user2/folder1/", "/documents/user2/folder1/*"),
-			new KeyValuePair(
-				"/documents/user2/folder2", "/documents/user2/folder2/*"),
-			new KeyValuePair(
-				"/documents/user2/folder2/", "/documents/user2/folder2/*"),
-			new KeyValuePair("/documents/user3", "/documents/*"),
-			new KeyValuePair("/documents/user3/", "/documents/*"),
-			new KeyValuePair("/documents/user3/folder1", "/documents/*"),
-			new KeyValuePair("/documents/user3/folder1/", "/documents/*"),
-			new KeyValuePair("/documents/user3/folder2", "/documents/*"),
-			new KeyValuePair("/documents/user3/folder2/", "/documents/*"),
-			new KeyValuePair("/test", "/*"), new KeyValuePair("/test/", "/*"),
-			new KeyValuePair("no/leading/slash", StringPool.BLANK),
-			new KeyValuePair("no/leading/slash/", StringPool.BLANK),
-			new KeyValuePair("no/leading/slash/*", "no/leading/slash/*"),
-			new KeyValuePair("no/leading/slash/test", StringPool.BLANK),
-			new KeyValuePair("test", StringPool.BLANK),
-			new KeyValuePair("test.jsp", "*.jsp"),
-			new KeyValuePair("test.jspf/", StringPool.BLANK),
-			new KeyValuePair("test/", StringPool.BLANK),
-			new KeyValuePair("test/main.jsp/*", StringPool.BLANK),
-			new KeyValuePair("test/main.jspf", "*.jspf")
-		};
-
-		for (KeyValuePair keyValuePair : keyValuePairs) {
-			CORSSupport corsSupport = new CORSSupport();
-
-			corsSupport.setHeader("pattern", keyValuePair.getValue());
-
-			corsSupports.put(keyValuePair.getValue(), corsSupport);
-		}
-
-		URLToCORSSupportMapper urlToCORSSupportMapper =
-			new URLToCORSSupportMapper(corsSupports);
-
-		for (KeyValuePair keyValuePair : keyValuePairs) {
-			CORSSupport corsSupport = urlToCORSSupportMapper.get(
-				keyValuePair.getKey());
-
-			if (corsSupport == null) {
-				Assert.assertEquals(StringPool.BLANK, keyValuePair.getValue());
-
-				continue;
-			}
-
-			Map<String, String> headers = new HashMap<>();
-
-			corsSupport.writeResponseHeaders(__ -> "origin", headers::put);
-
-			try {
-				Assert.assertEquals(
-					keyValuePair.getValue(), headers.get("pattern"));
-			}
-			catch (ComparisonFailure comparisonFailure) {
-				throw new ComparisonFailure(
-					"When testing " + keyValuePair.getKey() + ":",
-					comparisonFailure.getExpected(),
-					comparisonFailure.getActual());
-			}
-		}
+		super.testGetCorrectness(_urlToCORSSupportMapper);
 	}
+
+	private URLToCORSSupportMapper _urlToCORSSupportMapper;
 
 }
