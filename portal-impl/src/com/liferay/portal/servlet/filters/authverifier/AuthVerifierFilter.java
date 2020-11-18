dcb183b36c6541161de1a639b2d1771b651ab130
arthurchan35 <arthurchan35@gmail.com>
2020-11-18 12:14:12 +0800

    LPS-120270 Backward compatible with legacy url-patterns

--- portal-impl/src/com/liferay/portal/servlet/filters/authverifier/AuthVerifierFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/authverifier/AuthVerifierFilter.java
@@ -399,6 +399,35 @@ public class AuthVerifierFilter extends BasePortalFilter {
 			_rebuildAll();
 		}
 
+		/**
+		 * We have non-standard url-pattern across the portal for AuthVerifiers,
+		 * in order to be backward compatible, we need to do runtime fix, like:
+		 * "*" will be fixed to "/*"
+		 * "/abc*" will be fixed to "/abc/*"
+		 *
+		 * Notice both "*" and "/abc*" are valid url-pattern, but the ending "*"
+		 * is not considered as wildcard by servlet spec.
+		 *
+		 * In the future, all non-standard url-patterns need rewriting.
+		 */
+		private String _fixLegacyURLPattern(String urlPattern) {
+			if ((urlPattern == null) || (urlPattern.length() == 0)) {
+				return urlPattern;
+			}
+
+			if (urlPattern.charAt(urlPattern.length() - 1) != '*') {
+				return urlPattern;
+			}
+
+			if ((urlPattern.length() > 1) &&
+				(urlPattern.charAt(urlPattern.length() - 2) == '/')) {
+
+				return urlPattern;
+			}
+
+			return urlPattern.substring(0, urlPattern.length() - 1) + "/*";
+		}
+
 		private Properties _loadProperties(
 			ServiceReference<AuthVerifier> serviceReference,
 			String authVerifierClassName) {
@@ -519,6 +548,8 @@ public class AuthVerifierFilter extends BasePortalFilter {
 					properties.getProperty("urls.excludes"));
 
 				for (String urlsExclude : urlsExcludes) {
+					urlsExclude = _fixLegacyURLPattern(urlsExclude);
+
 					if (!excludeAuthVerifierConfigurations.containsKey(
 							urlsExclude)) {
 
@@ -538,6 +569,8 @@ public class AuthVerifierFilter extends BasePortalFilter {
 					properties.getProperty("urls.includes"));
 
 				for (String urlsInclude : urlsIncludes) {
+					urlsInclude = _fixLegacyURLPattern(urlsInclude);
+
 					if (!includeAuthVerifierConfigurations.containsKey(
 							urlsInclude)) {
 
