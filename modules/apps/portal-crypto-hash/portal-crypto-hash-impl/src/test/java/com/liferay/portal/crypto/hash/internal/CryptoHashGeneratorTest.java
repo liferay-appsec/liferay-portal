6df54587762d92ff33a97aa6f74770de0bb1af08
arthurchan35 <arthurchan35@gmail.com>
2021-02-08 12:38:58 +0800

    LPS-124770 Update test

--- modules/apps/portal-crypto-hash/portal-crypto-hash-impl/src/test/java/com/liferay/portal/crypto/hash/internal/CryptoHashGeneratorTest.java
+++ modules/apps/portal-crypto-hash/portal-crypto-hash-impl/src/test/java/com/liferay/portal/crypto/hash/internal/CryptoHashGeneratorTest.java
@@ -14,10 +14,14 @@
 
 package com.liferay.portal.crypto.hash.internal;
 
+import com.liferay.portal.crypto.hash.CryptoHashGenerationContext;
 import com.liferay.portal.crypto.hash.CryptoHashGenerator;
 import com.liferay.portal.crypto.hash.CryptoHashResponse;
+import com.liferay.portal.crypto.hash.CryptoHashVerificationContext;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.UnicodeFormatter;
+
+import java.util.HashMap;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -36,36 +40,75 @@ public class CryptoHashGeneratorTest {
 
 	@Test
 	public void testGenerate() throws Exception {
+		CryptoHashGenerationContext cryptoHashGenerationContext =
+			new CryptoHashGenerationContext(
+				_CRYPTO_HASH_PROVIDER_1, new HashMap<>(), -1);
+
 		CryptoHashResponse cryptoHashResponse = _cryptoHashGenerator.generate(
-			_INPUT);
+			_INPUT, cryptoHashGenerationContext);
+
+		CryptoHashVerificationContext cryptoHashVerificationContext =
+			new CryptoHashVerificationContext(
+				new byte[0], cryptoHashResponse.getSalt(),
+				_CRYPTO_HASH_PROVIDER_1, new HashMap<>());
 
 		Assert.assertFalse(
 			_cryptoHashGenerator.verify(
 				_randomBytes(), cryptoHashResponse.getHash(),
-				cryptoHashResponse.getSalt()));
+				cryptoHashVerificationContext));
 		Assert.assertTrue(
 			_cryptoHashGenerator.verify(
 				_INPUT, cryptoHashResponse.getHash(),
-				cryptoHashResponse.getSalt()));
+				cryptoHashVerificationContext));
 	}
 
+	/**
+	 * Example to show how to verify an INPUT against a HASH when the
+	 * HASH was previous generated from multiple contexts.
+	 */
 	@Test
 	public void testVerify() throws Exception {
-		_cryptoHashGenerator.verify(_INPUT, _HASH, _SALT);
+		CryptoHashVerificationContext[] cryptoHashVerificationContexts =
+			new CryptoHashVerificationContext[2];
+
+		cryptoHashVerificationContexts[0] = new CryptoHashVerificationContext(
+			_PEPPER, _SALT_1, _CRYPTO_HASH_PROVIDER_1, new HashMap<>());
+
+		cryptoHashVerificationContexts[1] = new CryptoHashVerificationContext(
+			_PEPPER, _SALT_2, _CRYPTO_HASH_PROVIDER_2, new HashMap<>());
+
+		Assert.assertFalse(
+			_cryptoHashGenerator.verify(
+				_randomBytes(), _HASH, cryptoHashVerificationContexts));
+
+		Assert.assertTrue(
+			_cryptoHashGenerator.verify(
+				_INPUT, _HASH, cryptoHashVerificationContexts));
 	}
 
-	private static byte[] _randomBytes() {
+	private byte[] _randomBytes() {
 		String string = RandomTestUtil.randomString();
 
 		return string.getBytes();
 	}
 
+	private static final String _CRYPTO_HASH_PROVIDER_1 = "SHA-256";
+
+	private static final String _CRYPTO_HASH_PROVIDER_2 = "SHA-512";
+
 	private static final byte[] _HASH = UnicodeFormatter.hexToBytes(
-		RandomTestUtil.randomString(128));
+		"ee765094649dcc6b5e89a91663cbeb80ecceed035e13201da471a97d30534f57" +
+			"1dd8974729feb4e1696485b1e054672d91c9e774514921c067028a46bcb6f1c5");
 
-	private static final byte[] _INPUT = _randomBytes();
+	private static final byte[] _INPUT = "password".getBytes();
 
-	private static final byte[] _SALT = _randomBytes();
+	// Stub pepper, real pepper implementation comes in next iteration
+
+	private static final byte[] _PEPPER = new byte[0];
+
+	private static final byte[] _SALT_1 = "salt1".getBytes();
+
+	private static final byte[] _SALT_2 = "salt2".getBytes();
 
 	private CryptoHashGenerator _cryptoHashGenerator;
 
