0d60f140b58c924ef3cec33c59cdd6d3dc5540d6
arthurchan35 <arthurchan35@gmail.com>
2021-03-09 12:17:26 +0800

    LPS-88439 Decouple Filter from SamlLoginAction:
    Currently there are multiple back and forward exchanges between SamlSpSssoFilter and SamlLoginAction, this commit decouples them.
    See next commits.

--- modules/dxp/apps/saml/saml-impl/src/main/java/com/liferay/saml/runtime/internal/servlet/filter/SamlSpSsoFilter.java
+++ modules/dxp/apps/saml/saml-impl/src/main/java/com/liferay/saml/runtime/internal/servlet/filter/SamlSpSsoFilter.java
@@ -27,13 +27,19 @@ import com.liferay.portal.kernel.util.Props;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.saml.constants.SamlWebKeys;
+import com.liferay.saml.persistence.model.SamlSpIdpConnection;
 import com.liferay.saml.persistence.model.SamlSpSession;
+import com.liferay.saml.persistence.service.SamlSpIdpConnectionLocalService;
 import com.liferay.saml.runtime.configuration.SamlProviderConfiguration;
 import com.liferay.saml.runtime.configuration.SamlProviderConfigurationHelper;
+import com.liferay.saml.runtime.servlet.profile.SamlSpIdpConnectionsProfile;
 import com.liferay.saml.runtime.servlet.profile.SingleLogoutProfile;
 import com.liferay.saml.runtime.servlet.profile.WebSsoProfile;
 import com.liferay.saml.util.SamlHttpRequestUtil;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -46,6 +52,9 @@ import javax.servlet.http.HttpSession;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Mika Koivisto
@@ -143,17 +152,23 @@ public class SamlSpSsoFilter extends BaseSamlPortalFilter {
 				_portal.getCurrentCompleteURL(httpServletRequest));
 		}
 		else if (requestPath.equals("/c/portal/login")) {
-			RequestDispatcher requestDispatcher =
-				_servletContext.getRequestDispatcher("/c/portal/saml/login");
+			List<SamlSpIdpConnection> enabledSamlSpIdpConnections =
+				_enabledSamlSpIdpConnections(httpServletRequest);
 
-			httpServletResponse.setContentType("text/html");
+			if (enabledSamlSpIdpConnections.size() > 1) {
+				RequestDispatcher requestDispatcher =
+					_servletContext.getRequestDispatcher(
+						"/c/portal/saml/login");
 
-			requestDispatcher.include(httpServletRequest, httpServletResponse);
+				httpServletResponse.setContentType("text/html");
 
-			Object samlSpIdpConnection = httpServletRequest.getAttribute(
-				SamlWebKeys.SAML_SP_IDP_CONNECTION);
+				requestDispatcher.include(
+					httpServletRequest, httpServletResponse);
 
-			if (samlSpIdpConnection != null) {
+				return;
+			}
+
+			if (enabledSamlSpIdpConnections.size() == 1) {
 				try {
 					login(httpServletRequest, httpServletResponse);
 				}
@@ -164,21 +179,15 @@ public class SamlSpSsoFilter extends BaseSamlPortalFilter {
 								portalException.getMessage());
 					}
 				}
+
+				return;
 			}
-			else {
-				SamlProviderConfiguration samlProviderConfiguration =
-					_samlProviderConfigurationHelper.
-						getSamlProviderConfiguration();
 
-				Object samlSsologinContext = httpServletRequest.getAttribute(
-					SamlWebKeys.SAML_SSO_LOGIN_CONTEXT);
+			SamlProviderConfiguration samlProviderConfiguration =
+				_samlProviderConfigurationHelper.getSamlProviderConfiguration();
 
-				if ((samlSsologinContext == null) &&
-					samlProviderConfiguration.allowShowingTheLoginPortlet()) {
-
-					filterChain.doFilter(
-						httpServletRequest, httpServletResponse);
-				}
+			if (samlProviderConfiguration.allowShowingTheLoginPortlet()) {
+				filterChain.doFilter(httpServletRequest, httpServletResponse);
 			}
 		}
 		else if (requestPath.equals("/c/portal/logout")) {
@@ -241,6 +250,35 @@ public class SamlSpSsoFilter extends BaseSamlPortalFilter {
 			httpServletRequest, httpServletResponse, relayState);
 	}
 
+	private List<SamlSpIdpConnection> _enabledSamlSpIdpConnections(
+		HttpServletRequest httpServletRequest) {
+
+		List<SamlSpIdpConnection> samlSpIdpConnections =
+			_samlSpIdpConnectionLocalService.getSamlSpIdpConnections(
+				_portal.getCompanyId(httpServletRequest));
+
+		Stream<SamlSpIdpConnection> stream = samlSpIdpConnections.stream();
+
+		return stream.filter(
+			samlSpIdpConnection -> _isEnabled(
+				samlSpIdpConnection, httpServletRequest)
+		).collect(
+			Collectors.toList()
+		);
+	}
+
+	private boolean _isEnabled(
+		SamlSpIdpConnection samlSpIdpConnection,
+		HttpServletRequest httpServletRequest) {
+
+		if (_samlSpIdpConnectionsProfile != null) {
+			return _samlSpIdpConnectionsProfile.isEnabled(
+				samlSpIdpConnection, httpServletRequest);
+		}
+
+		return samlSpIdpConnection.isEnabled();
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		SamlSpSsoFilter.class);
 
@@ -255,6 +293,16 @@ public class SamlSpSsoFilter extends BaseSamlPortalFilter {
 
 	@Reference
 	private SamlProviderConfigurationHelper _samlProviderConfigurationHelper;
+
+	@Reference
+	private SamlSpIdpConnectionLocalService _samlSpIdpConnectionLocalService;
+
+	@Reference(
+		cardinality = ReferenceCardinality.OPTIONAL,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY
+	)
+	private volatile SamlSpIdpConnectionsProfile _samlSpIdpConnectionsProfile;
 
 	private ServletContext _servletContext;
 
