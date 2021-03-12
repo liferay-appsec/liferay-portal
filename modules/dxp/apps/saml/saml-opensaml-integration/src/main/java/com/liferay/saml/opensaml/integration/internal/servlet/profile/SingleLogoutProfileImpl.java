84417bafbaeefd4c5863f73c98d27de6455a9d4d
arthurchan35 <arthurchan35@gmail.com>
2021-03-12 15:36:55 +0800

    LPS-130822 Simplify SingleLogoutProfile.processIdpLogout:
    SPFilter can do the init by itself, no need to tangle this logic in the method.

--- modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/servlet/profile/SingleLogoutProfileImpl.java
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/servlet/profile/SingleLogoutProfileImpl.java
@@ -123,6 +123,27 @@ public class SingleLogoutProfileImpl
 	extends BaseProfile implements SingleLogoutProfile {
 
 	@Override
+	public void initiateIdpSingleLogout(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		SamlSloContext samlSloContext = getSamlSloContext(
+			httpServletRequest, null);
+
+		if (samlSloContext != null) {
+			String redirect = StringBundler.concat(
+				portal.getPortalURL(httpServletRequest), portal.getPathMain(),
+				"/portal/saml/slo_logout");
+
+			httpServletResponse.sendRedirect(redirect);
+		}
+		else {
+			redirectToLogout(httpServletRequest, httpServletResponse);
+		}
+	}
+
+	@Override
 	public boolean isSingleLogoutSupported(
 		HttpServletRequest httpServletRequest) {
 
@@ -197,11 +218,7 @@ public class SingleLogoutProfileImpl
 			httpServletResponse.addHeader(
 				HttpHeaders.PRAGMA, HttpHeaders.PRAGMA_NO_CACHE_VALUE);
 
-			if (requestPath.equals("/c/portal/logout")) {
-				initiateIdpSingleLogout(
-					httpServletRequest, httpServletResponse);
-			}
-			else if (requestPath.equals("/c/portal/saml/slo_logout")) {
+			if (requestPath.equals("/c/portal/saml/slo_logout")) {
 				SamlSloContext samlSloContext = getSamlSloContext(
 					httpServletRequest, null);
 
@@ -494,26 +511,6 @@ public class SingleLogoutProfileImpl
 		}
 
 		return samlSloContext;
-	}
-
-	protected void initiateIdpSingleLogout(
-			HttpServletRequest httpServletRequest,
-			HttpServletResponse httpServletResponse)
-		throws Exception {
-
-		SamlSloContext samlSloContext = getSamlSloContext(
-			httpServletRequest, null);
-
-		if (samlSloContext != null) {
-			String redirect = StringBundler.concat(
-				portal.getPortalURL(httpServletRequest), portal.getPathMain(),
-				"/portal/saml/slo_logout");
-
-			httpServletResponse.sendRedirect(redirect);
-		}
-		else {
-			redirectToLogout(httpServletRequest, httpServletResponse);
-		}
 	}
 
 	protected void performIdpFinishLogout(
