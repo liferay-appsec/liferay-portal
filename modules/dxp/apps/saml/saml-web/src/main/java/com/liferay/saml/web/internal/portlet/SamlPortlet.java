82d77018fb94e207f6088d89aaa83c4d78420c5a
arthurchan35 <arthurchan35@gmail.com>
2021-04-13 13:40:35 +0800

    LPS-130822 I guess we dont need to migrate properties no more

--- modules/dxp/apps/saml/saml-web/src/main/java/com/liferay/saml/web/internal/portlet/SamlPortlet.java
+++ modules/dxp/apps/saml/saml-web/src/main/java/com/liferay/saml/web/internal/portlet/SamlPortlet.java
@@ -15,23 +15,11 @@
 package com.liferay.saml.web.internal.portlet;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.util.PropsUtil;
-import com.liferay.portal.util.PropsValues;
 import com.liferay.saml.constants.SamlPortletKeys;
-import com.liferay.saml.constants.SamlWebKeys;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 import javax.portlet.Portlet;
 
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 
 /**
  * @author Arthur Chan
@@ -53,51 +41,4 @@ import org.osgi.service.component.annotations.Deactivate;
 	service = Portlet.class
 )
 public class SamlPortlet extends MVCPortlet {
-
-	@Activate
-	protected void activate(Map<String, Object> properties) {
-		_loginDialogDisabled = GetterUtil.getBoolean(
-			PropsUtil.get(PropsKeys.LOGIN_DIALOG_DISABLED));
-
-		PropsUtil.set(PropsKeys.LOGIN_DIALOG_DISABLED, "true");
-
-		if (!PropsValues.SESSION_ENABLE_PHISHING_PROTECTION) {
-			return;
-		}
-
-		List<String> sessionPhishingProtectedAttributes = new ArrayList<>(
-			Arrays.asList(PropsValues.SESSION_PHISHING_PROTECTED_ATTRIBUTES));
-
-		sessionPhishingProtectedAttributes.add(SamlWebKeys.SAML_SP_SESSION_KEY);
-		sessionPhishingProtectedAttributes.add(
-			SamlWebKeys.SAML_SSO_REQUEST_CONTEXT);
-
-		PropsValues.SESSION_PHISHING_PROTECTED_ATTRIBUTES =
-			sessionPhishingProtectedAttributes.toArray(new String[0]);
-	}
-
-	@Deactivate
-	protected void deactivate() {
-		PropsUtil.set(
-			PropsKeys.LOGIN_DIALOG_DISABLED,
-			String.valueOf(_loginDialogDisabled));
-
-		if (!PropsValues.SESSION_ENABLE_PHISHING_PROTECTION) {
-			return;
-		}
-
-		List<String> sessionPhishingProtectedAttributes = new ArrayList<>(
-			Arrays.asList(PropsValues.SESSION_PHISHING_PROTECTED_ATTRIBUTES));
-
-		sessionPhishingProtectedAttributes.remove(
-			SamlWebKeys.SAML_SP_SESSION_KEY);
-		sessionPhishingProtectedAttributes.remove(
-			SamlWebKeys.SAML_SSO_REQUEST_CONTEXT);
-
-		PropsValues.SESSION_PHISHING_PROTECTED_ATTRIBUTES =
-			sessionPhishingProtectedAttributes.toArray(new String[0]);
-	}
-
-	private boolean _loginDialogDisabled;
-
 }
