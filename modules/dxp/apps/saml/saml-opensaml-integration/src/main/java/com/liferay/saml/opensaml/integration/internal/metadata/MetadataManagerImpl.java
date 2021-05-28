7e34a1f2c4352dc5d46a0ec5981e13de027f6b39
arthurchan35 <arthurchan35@gmail.com>
2021-05-28 13:45:46 +0800

    LPS-130822 Feed home URL

--- modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/metadata/MetadataManagerImpl.java
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/metadata/MetadataManagerImpl.java
@@ -183,18 +183,21 @@ public class MetadataManagerImpl
 				httpServletRequest,
 				isSSLRequired() || _portal.isSecure(httpServletRequest));
 			String localEntityId = _localEntityManager.getLocalEntityId();
+			String relativeHomeURL = _portal.getRelativeHomeURL(
+				httpServletRequest);
 
 			if (_samlProviderConfigurationHelper.isRoleIdp()) {
 				return MetadataGeneratorUtil.buildIdpEntityDescriptor(
-					portalURL, localEntityId, isWantAuthnRequestSigned(),
-					isSignMetadata(), getSigningCredential(),
-					encryptionCredential);
+					portalURL, relativeHomeURL, localEntityId,
+					isWantAuthnRequestSigned(), isSignMetadata(),
+					getSigningCredential(), encryptionCredential);
 			}
 			else if (_samlProviderConfigurationHelper.isRoleSp()) {
 				return MetadataGeneratorUtil.buildSpEntityDescriptor(
-					portalURL, localEntityId, isSignAuthnRequest(),
-					isSignMetadata(), isWantAssertionsSigned(),
-					getSigningCredential(), encryptionCredential);
+					portalURL, relativeHomeURL, localEntityId,
+					isSignAuthnRequest(), isSignMetadata(),
+					isWantAssertionsSigned(), getSigningCredential(),
+					encryptionCredential);
 			}
 
 			return null;
