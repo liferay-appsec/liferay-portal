5d7721c8a25b8671ba7680815d2c9cf5fab9d4d8
Michael Bowerman <michael.bowerman@liferay.com>
2022-07-06 08:37:30 -0700

    LPS-156599 Revert "LPS-142044 More accurate name"
    This reverts commit 90999150576905058b89829c04cbeafa39a98095.

--- modules/apps/portal-security/portal-security-ldap-impl/src/main/java/com/liferay/portal/security/ldap/internal/exportimport/LDAPUserImporterImpl.java
+++ modules/apps/portal-security/portal-security-ldap-impl/src/main/java/com/liferay/portal/security/ldap/internal/exportimport/LDAPUserImporterImpl.java
@@ -1029,7 +1029,7 @@ public class LDAPUserImporterImpl implements LDAPUserImporter, UserImporter {
 			role.getRoleId(), new long[] {group.getGroupId()});
 	}
 
-	private void _addUserGroupsNotBelongingToLDAPServer(
+	private void _addUserGroupsNotAddedByLDAPImport(
 			long userId, Set<Long> ldapServerGroupIds, Set<Long> userGroupIds)
 		throws Exception {
 
@@ -1462,7 +1462,7 @@ public class LDAPUserImporterImpl implements LDAPUserImporter, UserImporter {
 			}
 		}
 
-		_addUserGroupsNotBelongingToLDAPServer(
+		_addUserGroupsNotAddedByLDAPImport(
 			user.getUserId(), ldapServerGroupIds, newUserGroupIds);
 
 		Set<Long> oldUserGroupIds = new LinkedHashSet<>();
