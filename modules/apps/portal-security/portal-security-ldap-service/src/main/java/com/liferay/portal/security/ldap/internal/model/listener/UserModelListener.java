63bb4a7665ceda180d283f60bcde43a2eb911a61
Michael Bowerman <michael.bowerman@liferay.com>
2022-08-08 14:51:12 -0700

    LPS-156599 Add LDAPServerAttributeRels when adding/updating Users# new files yourself (see 'git help status').

--- modules/apps/portal-security/portal-security-ldap-service/src/main/java/com/liferay/portal/security/ldap/internal/model/listener/UserModelListener.java
+++ modules/apps/portal-security/portal-security-ldap-service/src/main/java/com/liferay/portal/security/ldap/internal/model/listener/UserModelListener.java
@@ -14,15 +14,58 @@
 
 package com.liferay.portal.security.ldap.internal.model.listener;
 
+import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.security.ldap.service.LDAPServerAttributeRelLocalService;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Michael Bowerman
  */
 @Component(immediate = true, service = ModelListener.class)
 public class UserModelListener extends BaseModelListener<User> {
+
+	@Override
+	public void onAfterCreate(User user) throws ModelListenerException {
+		long ldapServerId = user.getLdapServerId();
+
+		if (ldapServerId > 0) {
+			try {
+				_ldapServerAttributeRelLocalService.addLDAPServerAttributeRel(
+					ldapServerId, User.class.getName(), user.getUserId());
+			}
+			catch (Exception exception) {
+				throw new ModelListenerException(exception);
+			}
+		}
+	}
+
+	@Override
+	public void onAfterUpdate(User originalUser, User user)
+		throws ModelListenerException {
+
+		long ldapServerId = user.getLdapServerId();
+
+		if ((ldapServerId > 0) &&
+			!_ldapServerAttributeRelLocalService.hasLDAPServerAttributeRel(
+				ldapServerId, User.class.getName(), user.getUserId())) {
+
+			try {
+				_ldapServerAttributeRelLocalService.addLDAPServerAttributeRel(
+					ldapServerId, User.class.getName(), user.getUserId());
+			}
+			catch (Exception exception) {
+				throw new ModelListenerException(exception);
+			}
+		}
+	}
+
+	@Reference
+	private LDAPServerAttributeRelLocalService
+		_ldapServerAttributeRelLocalService;
+
 }
