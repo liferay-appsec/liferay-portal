df18cf28a6e729d7918e2ff4481ba13eb4172da0
arthurchan35 <arthurchan35@gmail.com>
2022-08-25 16:54:32 +0800

    LPS-157905 Extract common code

---
+++ modules/apps/oauth-resource-server/oauth-resource-server-impl/src/main/java/com/liferay/oauth/resource/server/internal/auth/verifier/BaseAuthVerifier.java
@@ -0,0 +1,225 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.oauth.resource.server.internal.auth.verifier;
+
+import com.liferay.oauth2.provider.constants.OAuth2ProviderConstants;
+import com.liferay.oauth2.provider.model.OAuth2Application;
+import com.liferay.oauth2.provider.model.OAuth2Authorization;
+import com.liferay.oauth2.provider.rest.spi.bearer.token.provider.BearerTokenProvider;
+import com.liferay.oauth2.provider.rest.spi.bearer.token.provider.BearerTokenProviderAccessor;
+import com.liferay.oauth2.provider.service.OAuth2ApplicationLocalService;
+import com.liferay.oauth2.provider.service.OAuth2ApplicationScopeAliasesLocalService;
+import com.liferay.oauth2.provider.service.OAuth2AuthorizationLocalService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.auth.AccessControlContext;
+import com.liferay.portal.kernel.security.auth.AuthException;
+import com.liferay.portal.kernel.security.auth.verifier.AuthVerifier;
+import com.liferay.portal.kernel.security.auth.verifier.AuthVerifierResult;
+import com.liferay.portal.kernel.servlet.HttpHeaders;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+
+/**
+ * @author Arthur Chan
+ */
+public abstract class BaseAuthVerifier implements AuthVerifier {
+
+	@Override
+	public AuthVerifierResult verify(
+			AccessControlContext accessControlContext, Properties properties)
+		throws AuthException {
+
+		AuthVerifierResult authVerifierResult = new AuthVerifierResult();
+
+		String bearerAccessToken = _getBearerAccessToken(
+			accessControlContext.getRequest());
+
+		if (bearerAccessToken == null) {
+			return authVerifierResult;
+		}
+
+		BearerTokenProvider.AccessToken liferayAccessToken =
+			_getLiferayAccessToken(bearerAccessToken);
+
+		HttpServletResponse httpServletResponse =
+			accessControlContext.getResponse();
+
+		if (liferayAccessToken == null) {
+			httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+			authVerifierResult.setState(
+				AuthVerifierResult.State.INVALID_CREDENTIALS);
+
+			return authVerifierResult;
+		}
+
+		OAuth2Application oAuth2Application =
+			liferayAccessToken.getOAuth2Application();
+
+		BearerTokenProvider bearerTokenProvider =
+			bearerTokenProviderAccessor.getBearerTokenProvider(
+				oAuth2Application.getCompanyId(),
+				oAuth2Application.getClientId());
+
+		if ((bearerTokenProvider == null) ||
+			!bearerTokenProvider.isValid(liferayAccessToken)) {
+
+			httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+			authVerifierResult.setState(
+				AuthVerifierResult.State.INVALID_CREDENTIALS);
+
+			return authVerifierResult;
+		}
+
+		authVerifierResult.setState(AuthVerifierResult.State.SUCCESS);
+
+		postProcess(liferayAccessToken, authVerifierResult);
+
+		return authVerifierResult;
+	}
+
+	protected abstract void postProcess(
+		BearerTokenProvider.AccessToken accessToken,
+		AuthVerifierResult authVerifierResult);
+
+	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY
+	)
+	protected volatile BearerTokenProviderAccessor bearerTokenProviderAccessor;
+
+	@Reference
+	protected OAuth2ApplicationLocalService oAuth2ApplicationLocalService;
+
+	@Reference
+	protected OAuth2ApplicationScopeAliasesLocalService
+		oAuth2ApplicationScopeAliasesLocalService;
+
+	@Reference
+	protected OAuth2AuthorizationLocalService oAuth2AuthorizationLocalService;
+
+	private String _getBearerAccessToken(
+		HttpServletRequest httpServletRequest) {
+
+		String authorization = httpServletRequest.getHeader(
+			HttpHeaders.AUTHORIZATION);
+
+		if (Validator.isBlank(authorization)) {
+			return null;
+		}
+
+		String[] authorizationParts = authorization.split("\\s");
+
+		String scheme = authorizationParts[0];
+
+		if (!StringUtil.equalsIgnoreCase(scheme, _TOKEN_TYPE)) {
+			return null;
+		}
+
+		if (authorizationParts.length < 2) {
+			return StringPool.BLANK;
+		}
+
+		return authorizationParts[1];
+	}
+
+	private BearerTokenProvider.AccessToken _getLiferayAccessToken(
+		String bearerAccessToken) {
+
+		if (Validator.isBlank(bearerAccessToken)) {
+			return null;
+		}
+
+		OAuth2Authorization oAuth2Authorization =
+			oAuth2AuthorizationLocalService.
+				fetchOAuth2AuthorizationByAccessTokenContent(bearerAccessToken);
+
+		if (oAuth2Authorization == null) {
+			return null;
+		}
+
+		bearerAccessToken = oAuth2Authorization.getAccessTokenContent();
+
+		if (OAuth2ProviderConstants.EXPIRED_TOKEN.equals(bearerAccessToken)) {
+			return null;
+		}
+
+		OAuth2Application oAuth2Application;
+
+		try {
+			oAuth2Application =
+				oAuth2ApplicationLocalService.getOAuth2Application(
+					oAuth2Authorization.getOAuth2ApplicationId());
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception);
+			}
+
+			return null;
+		}
+
+		Date createDate = oAuth2Authorization.getAccessTokenCreateDate();
+		Date expirationDate =
+			oAuth2Authorization.getAccessTokenExpirationDate();
+
+		long expiresIn =
+			(expirationDate.getTime() - createDate.getTime()) / 1000;
+
+		long issuedAt = createDate.getTime() / 1000;
+
+		List<String> scopeAliasesList = Collections.emptyList();
+
+		long oAuth2ApplicationScopeAliasesId =
+			oAuth2Authorization.getOAuth2ApplicationScopeAliasesId();
+
+		if (oAuth2ApplicationScopeAliasesId > 0) {
+			scopeAliasesList =
+				oAuth2ApplicationScopeAliasesLocalService.getScopeAliasesList(
+					oAuth2ApplicationScopeAliasesId);
+		}
+
+		return new BearerTokenProvider.AccessToken(
+			oAuth2Application, new ArrayList<>(), StringPool.BLANK, expiresIn,
+			new HashMap<>(), StringPool.BLANK, StringPool.BLANK, issuedAt,
+			StringPool.BLANK, StringPool.BLANK, new HashMap<>(),
+			StringPool.BLANK, StringPool.BLANK, scopeAliasesList,
+			bearerAccessToken, _TOKEN_TYPE, oAuth2Authorization.getUserId(),
+			oAuth2Authorization.getUserName());
+	}
+
+	private static final String _TOKEN_TYPE = "Bearer";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		BaseAuthVerifier.class);
+
+}
--- modules/apps/oauth-resource-server/oauth-resource-server-impl/src/main/java/com/liferay/oauth/resource/server/internal/jsonws/auth/verifier/OAuth2JSONWSAuthVerifier.java
+++ modules/apps/oauth-resource-server/oauth-resource-server-impl/src/main/java/com/liferay/oauth/resource/server/internal/jsonws/auth/verifier/OAuth2JSONWSAuthVerifier.java
@@ -14,49 +14,28 @@
 
 package com.liferay.oauth.resource.server.internal.jsonws.auth.verifier;
 
+import com.liferay.oauth.resource.server.internal.auth.verifier.BaseAuthVerifier;
 import com.liferay.oauth.resource.server.internal.jsonws.policy.scope.SAPEntryScope;
 import com.liferay.oauth.resource.server.internal.jsonws.policy.scope.SAPEntryScopeDescriptorFinderRegistrator;
-import com.liferay.oauth2.provider.constants.OAuth2ProviderConstants;
-import com.liferay.oauth2.provider.model.OAuth2Application;
 import com.liferay.oauth2.provider.model.OAuth2Authorization;
 import com.liferay.oauth2.provider.model.OAuth2ScopeGrant;
 import com.liferay.oauth2.provider.rest.spi.bearer.token.provider.BearerTokenProvider;
-import com.liferay.oauth2.provider.rest.spi.bearer.token.provider.BearerTokenProviderAccessor;
-import com.liferay.oauth2.provider.scope.liferay.ScopeLocator;
 import com.liferay.oauth2.provider.scope.liferay.constants.OAuth2ProviderScopeLiferayConstants;
 import com.liferay.oauth2.provider.scope.spi.scope.finder.ScopeFinder;
-import com.liferay.oauth2.provider.service.OAuth2ApplicationLocalService;
-import com.liferay.oauth2.provider.service.OAuth2ApplicationScopeAliasesLocalService;
-import com.liferay.oauth2.provider.service.OAuth2AuthorizationLocalService;
 import com.liferay.oauth2.provider.service.OAuth2ScopeGrantLocalService;
-import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.security.auth.AccessControlContext;
-import com.liferay.portal.kernel.security.auth.AuthException;
 import com.liferay.portal.kernel.security.auth.verifier.AuthVerifier;
 import com.liferay.portal.kernel.security.auth.verifier.AuthVerifierResult;
 import com.liferay.portal.kernel.security.service.access.policy.ServiceAccessPolicy;
-import com.liferay.portal.kernel.servlet.HttpHeaders;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.Validator;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.annotations.Component;
@@ -72,7 +51,7 @@ import org.osgi.service.component.annotations.ReferencePolicyOption;
 	property = "auth.verifier.OAuth2JSONWSAuthVerifier.urls.includes=/api/jsonws/*",
 	service = AuthVerifier.class
 )
-public class OAuth2JSONWSAuthVerifier implements AuthVerifier {
+public class OAuth2JSONWSAuthVerifier extends BaseAuthVerifier {
 
 	@Override
 	public String getAuthType() {
@@ -80,107 +59,54 @@ public class OAuth2JSONWSAuthVerifier implements AuthVerifier {
 	}
 
 	@Override
-	public AuthVerifierResult verify(
-			AccessControlContext accessControlContext, Properties properties)
-		throws AuthException {
-
-		AuthVerifierResult authVerifierResult = new AuthVerifierResult();
-
-		String accessTokenContent = _getAccessTokenContent(
-			accessControlContext);
-
-		if (accessTokenContent == null) {
-			return authVerifierResult;
-		}
+	public void postProcess(
+		BearerTokenProvider.AccessToken accessToken,
+		AuthVerifierResult authVerifierResult) {
 
 		OAuth2Authorization oAuth2Authorization =
-			_oAuth2AuthorizationLocalService.
+			oAuth2AuthorizationLocalService.
 				fetchOAuth2AuthorizationByAccessTokenContent(
-					accessTokenContent);
+					accessToken.getTokenKey());
 
-		try {
-			BearerTokenProvider.AccessToken accessToken = _getAccessToken(
-				oAuth2Authorization);
+		List<OAuth2ScopeGrant> oAuth2AuthorizationOAuth2ScopeGrants =
+			_oAuth2ScopeGrantLocalService.
+				getOAuth2AuthorizationOAuth2ScopeGrants(
+					oAuth2Authorization.getOAuth2AuthorizationId());
 
-			if (accessToken == null) {
-				HttpServletResponse httpServletResponse =
-					accessControlContext.getResponse();
+		Stream<OAuth2ScopeGrant> stream =
+			oAuth2AuthorizationOAuth2ScopeGrants.stream();
 
-				httpServletResponse.setStatus(
-					HttpServletResponse.SC_UNAUTHORIZED);
+		List<String> scopes = stream.filter(
+			oAuth2ScopeGrant -> _jaxRsApplicationNames.contains(
+				oAuth2ScopeGrant.getApplicationName())
+		).map(
+			OAuth2ScopeGrant::getScope
+		).collect(
+			Collectors.toList()
+		);
 
-				authVerifierResult.setState(
-					AuthVerifierResult.State.INVALID_CREDENTIALS);
+		List<SAPEntryScope> sapEntryScopes =
+			_sapEntryScopeDescriptorFinderRegistrator.
+				getRegisteredSAPEntryScopes(oAuth2Authorization.getCompanyId());
 
-				return authVerifierResult;
+		List<String> serviceAccessPolicyNames = new ArrayList<>(
+			sapEntryScopes.size());
+
+		for (SAPEntryScope sapEntryScope : sapEntryScopes) {
+			if (scopes.contains(sapEntryScope.getScope())) {
+				serviceAccessPolicyNames.add(sapEntryScope.getSAPEntryName());
 			}
-
-			OAuth2Application oAuth2Application =
-				accessToken.getOAuth2Application();
-
-			long companyId = oAuth2Application.getCompanyId();
-
-			BearerTokenProvider bearerTokenProvider =
-				_bearerTokenProviderAccessor.getBearerTokenProvider(
-					companyId, oAuth2Application.getClientId());
-
-			if ((bearerTokenProvider == null) ||
-				!bearerTokenProvider.isValid(accessToken)) {
-
-				return authVerifierResult;
-			}
-
-			List<OAuth2ScopeGrant> oAuth2AuthorizationOAuth2ScopeGrants =
-				_oAuth2ScopeGrantLocalService.
-					getOAuth2AuthorizationOAuth2ScopeGrants(
-						oAuth2Authorization.getOAuth2AuthorizationId());
-
-			Stream<OAuth2ScopeGrant> stream =
-				oAuth2AuthorizationOAuth2ScopeGrants.stream();
-
-			List<String> scopes = stream.filter(
-				oAuth2ScopeGrant -> _jaxRsApplicationNames.contains(
-					oAuth2ScopeGrant.getApplicationName())
-			).map(
-				OAuth2ScopeGrant::getScope
-			).collect(
-				Collectors.toList()
-			);
-
-			List<SAPEntryScope> sapEntryScopes =
-				_sapEntryScopeDescriptorFinderRegistrator.
-					getRegisteredSAPEntryScopes(companyId);
-
-			List<String> serviceAccessPolicyNames = new ArrayList<>(
-				sapEntryScopes.size());
-
-			for (SAPEntryScope sapEntryScope : sapEntryScopes) {
-				if (scopes.contains(sapEntryScope.getScope())) {
-					serviceAccessPolicyNames.add(
-						sapEntryScope.getSAPEntryName());
-				}
-			}
-
-			Map<String, Object> settings = authVerifierResult.getSettings();
-
-			settings.put(
-				BearerTokenProvider.AccessToken.class.getName(), accessToken);
-			settings.put(
-				ServiceAccessPolicy.SERVICE_ACCESS_POLICY_NAMES,
-				serviceAccessPolicyNames);
-
-			authVerifierResult.setState(AuthVerifierResult.State.SUCCESS);
-			authVerifierResult.setUserId(accessToken.getUserId());
-
-			return authVerifierResult;
 		}
-		catch (Exception exception) {
-			if (_log.isDebugEnabled()) {
-				_log.debug("Unable to verify OAuth2 access token", exception);
-			}
 
-			return authVerifierResult;
-		}
+		Map<String, Object> settings = authVerifierResult.getSettings();
+
+		settings.put(
+			BearerTokenProvider.AccessToken.class.getName(), accessToken);
+		settings.put(
+			ServiceAccessPolicy.SERVICE_ACCESS_POLICY_NAMES,
+			serviceAccessPolicyNames);
+
+		authVerifierResult.setUserId(accessToken.getUserId());
 	}
 
 	@Reference(
@@ -205,104 +131,8 @@ public class OAuth2JSONWSAuthVerifier implements AuthVerifier {
 				serviceReference.getProperty("osgi.jaxrs.name")));
 	}
 
-	private BearerTokenProvider.AccessToken _getAccessToken(
-			OAuth2Authorization oAuth2Authorization)
-		throws PortalException {
-
-		if (oAuth2Authorization == null) {
-			return null;
-		}
-
-		String accessTokenContent = oAuth2Authorization.getAccessTokenContent();
-
-		if (OAuth2ProviderConstants.EXPIRED_TOKEN.equals(accessTokenContent)) {
-			return null;
-		}
-
-		OAuth2Application oAuth2Application =
-			_oAuth2ApplicationLocalService.getOAuth2Application(
-				oAuth2Authorization.getOAuth2ApplicationId());
-
-		Date createDate = oAuth2Authorization.getAccessTokenCreateDate();
-		Date expirationDate =
-			oAuth2Authorization.getAccessTokenExpirationDate();
-
-		long expiresIn =
-			(expirationDate.getTime() - createDate.getTime()) / 1000;
-
-		long issuedAt = createDate.getTime() / 1000;
-
-		List<String> scopeAliasesList = Collections.emptyList();
-
-		long oAuth2ApplicationScopeAliasesId =
-			oAuth2Authorization.getOAuth2ApplicationScopeAliasesId();
-
-		if (oAuth2ApplicationScopeAliasesId > 0) {
-			scopeAliasesList =
-				_oAuth2ApplicationScopeAliasesLocalService.getScopeAliasesList(
-					oAuth2ApplicationScopeAliasesId);
-		}
-
-		return new BearerTokenProvider.AccessToken(
-			oAuth2Application, new ArrayList<>(), StringPool.BLANK, expiresIn,
-			new HashMap<>(), StringPool.BLANK, StringPool.BLANK, issuedAt,
-			StringPool.BLANK, StringPool.BLANK, new HashMap<>(),
-			StringPool.BLANK, StringPool.BLANK, scopeAliasesList,
-			accessTokenContent, _TOKEN_KEY, oAuth2Authorization.getUserId(),
-			oAuth2Authorization.getUserName());
-	}
-
-	private String _getAccessTokenContent(
-		AccessControlContext accessControlContext) {
-
-		HttpServletRequest httpServletRequest =
-			accessControlContext.getRequest();
-
-		String authorization = httpServletRequest.getHeader(
-			HttpHeaders.AUTHORIZATION);
-
-		if (Validator.isBlank(authorization)) {
-			return null;
-		}
-
-		String[] authorizationParts = authorization.split("\\s");
-
-		String scheme = authorizationParts[0];
-
-		if (!StringUtil.equalsIgnoreCase(scheme, _TOKEN_KEY)) {
-			return null;
-		}
-
-		if (authorizationParts.length < 2) {
-			return StringPool.BLANK;
-		}
-
-		return authorizationParts[1];
-	}
-
-	private static final String _TOKEN_KEY = "Bearer";
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		OAuth2JSONWSAuthVerifier.class);
-
-	@Reference(
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY
-	)
-	private volatile BearerTokenProviderAccessor _bearerTokenProviderAccessor;
-
 	private final Set<String> _jaxRsApplicationNames =
 		Collections.newSetFromMap(new ConcurrentHashMap<>());
-
-	@Reference
-	private OAuth2ApplicationLocalService _oAuth2ApplicationLocalService;
-
-	@Reference
-	private OAuth2ApplicationScopeAliasesLocalService
-		_oAuth2ApplicationScopeAliasesLocalService;
-
-	@Reference
-	private OAuth2AuthorizationLocalService _oAuth2AuthorizationLocalService;
 
 	@Reference
 	private OAuth2ScopeGrantLocalService _oAuth2ScopeGrantLocalService;
@@ -310,8 +140,5 @@ public class OAuth2JSONWSAuthVerifier implements AuthVerifier {
 	@Reference
 	private SAPEntryScopeDescriptorFinderRegistrator
 		_sapEntryScopeDescriptorFinderRegistrator;
-
-	@Reference
-	private ScopeLocator _scopeLocator;
 
 }
