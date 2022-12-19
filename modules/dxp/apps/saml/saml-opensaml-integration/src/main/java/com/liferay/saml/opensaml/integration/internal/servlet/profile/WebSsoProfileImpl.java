7a6d69178ecdc7e5678edac93c0cd0aa9bd19bfb
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2022-12-19 10:14:04 +0000

    LPS-88244 SF not used

--- modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/servlet/profile/WebSsoProfileImpl.java
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/servlet/profile/WebSsoProfileImpl.java
@@ -613,9 +613,7 @@ public class WebSsoProfileImpl extends BaseProfile implements WebSsoProfile {
 	}
 
 	protected Subject getSuccessSubject(
-		SamlSsoRequestContext samlSsoRequestContext,
-		AssertionConsumerService assertionConsumerService, NameID nameID,
-		SubjectConfirmationData subjectConfirmationData) {
+		NameID nameID, SubjectConfirmationData subjectConfirmationData) {
 
 		SubjectConfirmation subjectConfirmation =
 			OpenSamlUtil.buildSubjectConfirmation();
@@ -1260,9 +1258,7 @@ public class WebSsoProfileImpl extends BaseProfile implements WebSsoProfile {
 			OpenSamlUtil.buildIssuer(samlSelfEntityContext.getEntityId()));
 
 		assertion.setSubject(
-			getSuccessSubject(
-				samlSsoRequestContext, assertionConsumerService, nameID,
-				subjectConfirmationData));
+			getSuccessSubject(nameID, subjectConfirmationData));
 		assertion.setVersion(SAMLVersion.VERSION_20);
 
 		List<AuthnStatement> authnStatements = assertion.getAuthnStatements();
