dbdaee9ce1da946e19edab1b322e6427f98fcbfc
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2023-07-26 18:36:43 +0200

    LPS-175308 SF rename. Follow pattern from "imagemagick".

--- modules/apps/portal-security/portal-security-password-encryptor-impl/src/main/java/com/liferay/portal/security/password/encryptor/internal/PBKDF2PasswordEncryptor.java
+++ modules/apps/portal-security/portal-security-password-encryptor-impl/src/main/java/com/liferay/portal/security/password/encryptor/internal/PBKDF2PasswordEncryptor.java
@@ -65,7 +65,7 @@ public class PBKDF2PasswordEncryptor
 
 			if (GetterUtil.getBoolean(
 					PropsUtil.get(
-						PropsKeys.PASSWORDS_ENCRYPTION_ENABLE_BOUNCY_CASTLE))) {
+						PropsKeys.PASSWORDS_ENCRYPTION_BOUNCYCASTLE_ENABLED))) {
 
 				PKCS5S2ParametersGenerator generator =
 					new PKCS5S2ParametersGenerator();
