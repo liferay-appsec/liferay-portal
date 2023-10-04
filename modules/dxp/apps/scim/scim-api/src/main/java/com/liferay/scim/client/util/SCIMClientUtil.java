1b22d9e762944d106a3ed5bb74011a382ef9f61b
Rafael Praxedes <rafael.praxedes@liferay.com>
2023-10-04 11:19:07 -0300

    LPS-97345 Move configuration class to an api module so that it can be referenced on the scim impl module

---
+++ modules/dxp/apps/scim/scim-api/src/main/java/com/liferay/scim/client/util/SCIMClientUtil.java
@@ -0,0 +1,24 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.scim.client.util;
+
+import com.liferay.petra.string.CharPool;
+import com.liferay.portal.kernel.util.StringUtil;
+
+/**
+ * @author Rafael Praxedes
+ */
+public class SCIMClientUtil {
+
+	public static String generateSCIMClientId(String applicationName) {
+		String clientId = StringUtil.replace(
+			StringUtil.toLowerCase(applicationName), CharPool.SPACE,
+			CharPool.DASH);
+
+		return "SCIM_" + clientId;
+	}
+
+}
