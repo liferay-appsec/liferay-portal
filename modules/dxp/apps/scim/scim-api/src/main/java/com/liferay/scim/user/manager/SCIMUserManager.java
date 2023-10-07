14391a15aad1d8eb72f44b29efc4f1d90b3700aa
Rafael Praxedes <rafael.praxedes@liferay.com>
2023-10-07 03:17:44 +0100

    LPS-97345 Create the SCIMUserManager interface to represent the liferay SCIM business logic layer. It is required to keep the business logic separated from the SCIM protocol implementation

---
+++ modules/dxp/apps/scim/scim-api/src/main/java/com/liferay/scim/user/manager/SCIMUserManager.java
@@ -0,0 +1,23 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.scim.user.manager;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * @author Rafael Praxedes
+ */
+@ProviderType
+public interface SCIMUserManager {
+
+	public SCIMUser addOrUpdateSCIMUser(SCIMUser scimUser)
+		throws PortalException;
+
+	public SCIMUser fetchSCIMUser(long companyId, long userId);
+
+}
