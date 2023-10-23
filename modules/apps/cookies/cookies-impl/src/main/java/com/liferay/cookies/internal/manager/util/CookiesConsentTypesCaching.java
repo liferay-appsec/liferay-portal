1d3fff7dabf5c285f97ce39f60f304aab477ff9a
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-10-23 14:51:49 -0300

    LPS-176164 SF

--- modules/apps/cookies/cookies-impl/src/main/java/com/liferay/cookies/internal/manager/util/CookiesConsentTypesCaching.java
+++ modules/apps/cookies/cookies-impl/src/main/java/com/liferay/cookies/internal/manager/util/CookiesConsentTypesCaching.java
@@ -2,34 +2,35 @@
  * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
- 
+
 package com.liferay.cookies.internal.manager.util;
+
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import com.liferay.portal.kernel.util.Validator;
 
 /**
  * @author Mirna Gama
  */
 public class CookiesConsentTypesCaching {
-	 
-	private static Map<String, Integer> cookiesConsentTypesMapCache = 
-			new HashMap<String, Integer>();
-	
+
 	public static void addCookie(String cookieName, Integer cookieConsentType) {
 		if (!_isPresent(cookieName)) {
-			cookiesConsentTypesMapCache.put(cookieName, cookieConsentType);
+			_cookiesConsentTypesMapCache.put(cookieName, cookieConsentType);
 		}
 	}
 
-	private static boolean _isPresent(String cookieName) {
-		return !Validator.isNull(cookiesConsentTypesMapCache.get(cookieName));
+	public static Map<String, Integer> getCookiesConsentTypesMapCache() {
+		return _cookiesConsentTypesMapCache;
 	}
 
-	public static Map<String, Integer> getCookiesConsentTypesMapCache() {
-		return cookiesConsentTypesMapCache;
+	private static boolean _isPresent(String cookieName) {
+		return Validator.isNotNull(
+			_cookiesConsentTypesMapCache.get(cookieName));
 	}
+
+	private static final Map<String, Integer> _cookiesConsentTypesMapCache =
+		new HashMap<>();
 
 }
