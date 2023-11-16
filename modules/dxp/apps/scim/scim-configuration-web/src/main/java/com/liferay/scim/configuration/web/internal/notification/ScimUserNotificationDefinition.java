5897bea38ea1a364d104fb99208a0af452c41d8d
alvarosaugarlr <alvaro.saugar@liferay.com>
2023-11-16 16:54:16 +0100

    LPD-6757 notification definition and handler

---
+++ modules/dxp/apps/scim/scim-configuration-web/src/main/java/com/liferay/scim/configuration/web/internal/notification/ScimUserNotificationDefinition.java
@@ -0,0 +1,33 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.scim.configuration.web.internal.notification;
+
+import com.liferay.portal.kernel.model.MembershipRequestConstants;
+import com.liferay.portal.kernel.model.UserNotificationDeliveryConstants;
+import com.liferay.portal.kernel.notifications.UserNotificationDefinition;
+import com.liferay.portal.kernel.notifications.UserNotificationDeliveryType;
+import com.liferay.scim.configuration.web.internal.constants.ScimWebKeys;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alvaro Saugar
+ */
+@Component(service = UserNotificationDefinition.class)
+public class ScimUserNotificationDefinition extends UserNotificationDefinition {
+
+	public ScimUserNotificationDefinition() {
+		super(
+			ScimWebKeys.SCIM_CONFIGURATION, 0,
+			MembershipRequestConstants.STATUS_PENDING, "scim-email-subject");
+
+		addUserNotificationDeliveryType(
+			new UserNotificationDeliveryType(
+				"website", UserNotificationDeliveryConstants.TYPE_WEBSITE, true,
+				true));
+	}
+
+}
---
+++ modules/dxp/apps/scim/scim-configuration-web/src/main/java/com/liferay/scim/configuration/web/internal/notification/ScimUserNotificationHandler.java
@@ -0,0 +1,62 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.scim.configuration.web.internal.notification;
+
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.UserNotificationEvent;
+import com.liferay.portal.kernel.notifications.BaseUserNotificationHandler;
+import com.liferay.portal.kernel.notifications.UserNotificationHandler;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.service.UserNotificationEventLocalService;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.scim.configuration.web.internal.constants.ScimWebKeys;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alvaro Saugar
+ */
+@Component(service = UserNotificationHandler.class)
+public class ScimUserNotificationHandler extends BaseUserNotificationHandler {
+
+	public ScimUserNotificationHandler() {
+		setActionable(true);
+		setPortletId(ScimWebKeys.SCIM_CONFIGURATION);
+	}
+
+	@Override
+	protected String getBody(
+			UserNotificationEvent userNotificationEvent,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		JSONObject jsonObject = _jsonFactory.createJSONObject(
+			userNotificationEvent.getPayload());
+
+		return jsonObject.getString("body");
+	}
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+	@Reference
+	private UserNotificationEventLocalService
+		_userNotificationEventLocalService;
+
+}
