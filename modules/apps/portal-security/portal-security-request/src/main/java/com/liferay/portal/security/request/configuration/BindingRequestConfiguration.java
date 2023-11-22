60fd729aeea68163a7e474c01873b73b64135e22
alvarosaugarlr <alvaro.saugar@liferay.com>
2023-11-22 12:38:15 +0100

    LPS-196299 module and configuration

---
+++ modules/apps/portal-security/portal-security-request/src/main/java/com/liferay/portal/security/request/configuration/BindingRequestConfiguration.java
@@ -0,0 +1,29 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.security.request.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Alvaro Saugar
+ */
+@ExtendedObjectClassDefinition(
+	category = "security-tools", 
+	scope = ExtendedObjectClassDefinition.Scope.COMPANY
+)
+@Meta.OCD(
+	id = "com.liferay.portal.security.request.configuration.BindingRequestConfiguration",
+	localization = "content/Language",
+	name = "binding-request-configuration-name"
+)
+public interface BindingRequestConfiguration {
+
+	@Meta.AD(deflt = "true", name = "enabled", required = false)
+	public boolean enabled();
+
+}
