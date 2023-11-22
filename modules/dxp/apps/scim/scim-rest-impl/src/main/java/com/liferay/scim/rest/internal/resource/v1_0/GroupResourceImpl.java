de8772eb1c98987da0774544981a4ddeee143ade
Olivér Kecskeméty <oliver.kecskemety@liferay.com>
2023-11-22 11:10:10 +0100

    LPS-200030 Set endpoint of Group resources

--- modules/dxp/apps/scim/scim-rest-impl/src/main/java/com/liferay/scim/rest/internal/resource/v1_0/GroupResourceImpl.java
+++ modules/dxp/apps/scim/scim-rest-impl/src/main/java/com/liferay/scim/rest/internal/resource/v1_0/GroupResourceImpl.java
@@ -17,6 +17,7 @@ import com.liferay.scim.rest.dto.v1_0.GroupSchema;
 import com.liferay.scim.rest.internal.manager.UserManagerImpl;
 import com.liferay.scim.rest.resource.v1_0.GroupResource;
 
+import java.util.Collections;
 import java.util.Map;
 
 import javax.ws.rs.core.Response;
@@ -29,7 +30,9 @@ import org.osgi.service.component.annotations.ServiceScope;
 
 import org.wso2.charon3.core.extensions.UserManager;
 import org.wso2.charon3.core.protocol.SCIMResponse;
+import org.wso2.charon3.core.protocol.endpoints.AbstractResourceManager;
 import org.wso2.charon3.core.protocol.endpoints.GroupResourceManager;
+import org.wso2.charon3.core.schema.SCIMConstants;
 
 /**
  * @author Olivér Kecskeméty
@@ -105,6 +108,12 @@ public class GroupResourceImpl extends BaseGroupResourceImpl {
 
 	private static final GroupResourceManager _groupResourceManager =
 		new GroupResourceManager();
+
+	static {
+		AbstractResourceManager.setEndpointURLMap(
+			Collections.singletonMap(
+				SCIMConstants.GROUP_ENDPOINT, "/o/scim/Groups"));
+	}
 
 	@Reference
 	private ClassNameLocalService _classNameLocalService;
