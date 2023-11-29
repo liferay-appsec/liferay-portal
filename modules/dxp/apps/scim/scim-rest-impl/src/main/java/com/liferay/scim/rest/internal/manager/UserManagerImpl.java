b7af168a3b1ef230fcacc654723c42051c98183a
Olivér Kecskeméty <oliver.kecskemety@liferay.com>
2023-11-29 22:52:40 +0100

    LPS-200030 Fix and properly name function as it can return null

--- modules/dxp/apps/scim/scim-rest-impl/src/main/java/com/liferay/scim/rest/internal/manager/UserManagerImpl.java
+++ modules/dxp/apps/scim/scim-rest-impl/src/main/java/com/liferay/scim/rest/internal/manager/UserManagerImpl.java
@@ -481,7 +481,7 @@ public class UserManagerImpl implements UserManager {
 				_getScimClientOAuth2ApplicationConfiguration(
 					company.getCompanyId());
 
-		UserGroup userGroup = _getUserGroup(
+		UserGroup userGroup = _fetchUserGroup(
 			company.getCompanyId(), group.getExternalId(),
 			GetterUtil.getLong(group.getId()));
 
@@ -603,6 +603,20 @@ public class UserManagerImpl implements UserManager {
 		}
 
 		return null;
+	}
+
+	private UserGroup _fetchUserGroup(
+		long companyId, String externalReferenceCode, long groupId) {
+
+		UserGroup userGroup =
+			_userGroupLocalService.fetchUserGroupByExternalReferenceCode(
+				externalReferenceCode, companyId);
+
+		if (userGroup != null) {
+			return userGroup;
+		}
+
+		return _userGroupLocalService.fetchUserGroup(groupId);
 	}
 
 	private String _getScimClientId(
@@ -772,22 +786,6 @@ public class UserManagerImpl implements UserManager {
 		}
 
 		return userGroup;
-	}
-
-	private UserGroup _getUserGroup(
-			long companyId, String externalReferenceCode, long groupId)
-		throws AbstractCharonException {
-
-		UserGroup userGroup =
-			_userGroupLocalService.fetchUserGroupByExternalReferenceCode(
-				externalReferenceCode, companyId);
-
-		if (userGroup != null) {
-			throw new NotFoundException(
-				"No group found with group ID " + groupId);
-		}
-
-		return _getUserGroup(companyId, groupId);
 	}
 
 	private void _saveScimClientId(
