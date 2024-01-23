cc6a0c7f7d93a4a92af35e48961da38d340f0f7b
alvarosaugarlr <alvaro.saugar@liferay.com>
2024-01-23 14:20:14 +0100

    LPD-4254 adding the new behavior

--- portal-impl/src/com/liferay/portal/events/SetupAdminAutoLogin.java
+++ portal-impl/src/com/liferay/portal/events/SetupAdminAutoLogin.java
@@ -11,6 +11,7 @@ import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.auto.login.AutoLoginException;
 import com.liferay.portal.kernel.security.auto.login.BaseAutoLogin;
+import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
@@ -44,7 +45,9 @@ public class SetupAdminAutoLogin extends BaseAutoLogin {
 			HttpServletResponse httpServletResponse)
 		throws Exception {
 
-		if (Validator.isNotNull(PropsValues.DEFAULT_ADMIN_PASSWORD)) {
+		if (Validator.isNotNull(PropsValues.DEFAULT_ADMIN_PASSWORD) &&
+			!PropsValues.DEFAULT_ADMIN_PASSWORD.equals(Constants.TEST)) {
+
 			return null;
 		}
 
--- portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
@@ -99,6 +99,7 @@ import com.liferay.portal.kernel.transaction.TransactionConfig;
 import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
 import com.liferay.portal.kernel.transaction.Transactional;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleThreadLocal;
@@ -280,11 +281,15 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 
 					updatedCompany = _checkCompany(updatedCompany);
 
+					String defaulAdminPassword = null;
+
+					if (!PropsValues.DEFAULT_ADMIN_PASSWORD.equals(Constants.TEST)) {
+						defaulAdminPassword = GetterUtil.getString(
+							defaultAdminPassword, PropsValues.DEFAULT_ADMIN_PASSWORD);
+					}
+
 					_userLocalService.addDefaultAdminUser(
-						updatedCompany.getCompanyId(),
-						GetterUtil.getString(
-							defaultAdminPassword,
-							PropsValues.DEFAULT_ADMIN_PASSWORD),
+						updatedCompany.getCompanyId(), defaulAdminPassword,
 						GetterUtil.getString(
 							defaultAdminScreenName,
 							PropsValues.DEFAULT_ADMIN_SCREEN_NAME),
