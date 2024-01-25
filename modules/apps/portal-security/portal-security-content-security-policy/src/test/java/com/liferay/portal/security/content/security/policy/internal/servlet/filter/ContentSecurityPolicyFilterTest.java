8ed31771524186f47df9bb533477d1d93eb788b3
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2024-01-25 17:50:47 +0000

    LPS-206370 Test content is wellformed after nonce injection

---
+++ modules/apps/portal-security/portal-security-content-security-policy/src/test/java/com/liferay/portal/security/content/security/policy/internal/servlet/filter/ContentSecurityPolicyFilterTest.java
@@ -0,0 +1,86 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.security.content.security.policy.internal.servlet.filter;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Stian Sigvartsen
+ */
+public class ContentSecurityPolicyFilterTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testHTMLContent() {
+		Assert.assertEquals(
+			StringBundler.concat(
+				_HTML_CONTEXT_START,
+				String.join(" nonce=\"" + _NONCE + "\"", _HTML_CONTENT),
+				"</body></html>"),
+			_contentSecurityPolicyFilter.rewriteContent(
+				_NONCE,
+				StringBundler.concat(
+					"<html><body>",
+					String.join(StringPool.BLANK, _HTML_CONTENT),
+					_HTML_CONTEXT_END)));
+	}
+
+	@Test
+	public void testJSProvidedHTMLContent() {
+		ContentSecurityPolicyFilter contentSecurityPolicyFilter =
+			new ContentSecurityPolicyFilter();
+
+		String jsObjectLiteralContextStart =
+			"<script>index.render('', {\\\"portletId\\\":\\\"xyz\\\", " +
+				"\\\"content\\\":\\\"";
+		String jsObjectLiteralContextEnd = "\\\"}, 'xyz');</script>";
+
+		Assert.assertEquals(
+			StringBundler.concat(
+				_HTML_CONTEXT_START, jsObjectLiteralContextStart,
+				_escapeJS(
+					String.join(" nonce=\"" + _NONCE + "\"", _HTML_CONTENT)),
+				jsObjectLiteralContextEnd, _HTML_CONTEXT_END),
+			contentSecurityPolicyFilter.rewriteContent(
+				_NONCE,
+				StringBundler.concat(
+					_HTML_CONTEXT_START, jsObjectLiteralContextStart,
+					_escapeJS(String.join(StringPool.BLANK, _HTML_CONTENT)),
+					jsObjectLiteralContextEnd, _HTML_CONTEXT_END)));
+	}
+
+	private String _escapeJS(String content) {
+		return content.replaceAll("\"", "\\\\\"");
+	}
+
+	private static final String[] _HTML_CONTENT = {
+		"<div id=\"fragment-x\"><div class=\"\">TEXTNODE</div></div><style",
+		">.someclass{tmax-width: 100%;}</style>"
+	};
+
+	private static final String _HTML_CONTEXT_END = "</body></html>";
+
+	private static final String _HTML_CONTEXT_START = "<html><body>";
+
+	// Reserved RegEx characters. Should add '$', '\' also
+
+	private static final String _NONCE = "NONCE_.+*?^()[]{}|";
+
+	private final ContentSecurityPolicyFilter _contentSecurityPolicyFilter =
+		new ContentSecurityPolicyFilter();
+
+}
