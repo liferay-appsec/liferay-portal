f8553809a8f619f556cd8c0370a5109928a67560
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2024-02-23 18:55:16 +0000

    LPD-15723 SF inline

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -3817,11 +3817,9 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 			throw new UserEmailAddressException.MustNotBeNull();
 		}
 
-		User user = userPersistence.findByC_EA(companyId, emailAddress);
-
 		sendEmailAccountCreationAttemptNotification(
-			user, companyId, fromName, fromAddress, subject, body,
-			serviceContext);
+			userPersistence.findByC_EA(companyId, emailAddress), companyId,
+			fromName, fromAddress, subject, body, serviceContext);
 
 		return false;
 	}
