9ce788d53d97146ac7be830eb8b059ec94eb0538
alvarosaugarlr <alvaro.saugar@liferay.com>
2024-03-18 10:26:05 +0100

    LPD-6869 create Create Account Utility Page

--- portal-kernel/src/com/liferay/layout/utility/page/kernel/constants/LayoutUtilityPageEntryConstants.java
+++ portal-kernel/src/com/liferay/layout/utility/page/kernel/constants/LayoutUtilityPageEntryConstants.java
@@ -17,6 +17,8 @@ public class LayoutUtilityPageEntryConstants {
 
 	public static final String TYPE_SC_NOT_FOUND = "SC_NOT_FOUND";
 
+	public static final String TYPE_CREATE_ACCOUNT = "CREATE_ACCOUNT";
+
 	public static final String TYPE_STATUS = "STATUS";
 
 	public static final String TYPE_TERMS_OF_USE = "TERMS_OF_USE";
