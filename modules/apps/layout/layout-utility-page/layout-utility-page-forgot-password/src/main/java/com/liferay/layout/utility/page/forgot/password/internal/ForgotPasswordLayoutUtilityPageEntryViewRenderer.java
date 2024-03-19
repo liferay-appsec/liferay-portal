f50af93670f7dd4f3a3fe25c3286eb712236020d
alvarosaugarlr <alvaro.saugar@liferay.com>
2024-03-19 02:53:14 +0100

    LPD-18040 Add feature flag LPD-6871 story LPD-17439 epic LPD-6378 initiative

--- modules/apps/layout/layout-utility-page/layout-utility-page-forgot-password/src/main/java/com/liferay/layout/utility/page/forgot/password/internal/ForgotPasswordLayoutUtilityPageEntryViewRenderer.java
+++ modules/apps/layout/layout-utility-page/layout-utility-page-forgot-password/src/main/java/com/liferay/layout/utility/page/forgot/password/internal/ForgotPasswordLayoutUtilityPageEntryViewRenderer.java
@@ -19,27 +19,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-
 /**
  * @author Alvaro Saugar
  */
-@Component(
-	property = "utility.page.type=" + LayoutUtilityPageEntryConstants.TYPE_FORGOT_PASSWORD,
-	service = LayoutUtilityPageEntryViewRenderer.class
-)
 public class ForgotPasswordLayoutUtilityPageEntryViewRenderer
 	implements LayoutUtilityPageEntryViewRenderer {
-	/*
-		public ForgotPasswordLayoutUtilityPageEntryViewRenderer(
-			Language language, ServletContext servletContext) {
 
-			_language = language;
-			_servletContext = servletContext;
-		}
+	public ForgotPasswordLayoutUtilityPageEntryViewRenderer(
+		Language language, ServletContext servletContext) {
 
-	 */
+		_language = language;
+		_servletContext = servletContext;
+	}
+
 	@Override
 	public String getLabel(Locale locale) {
 		return _language.get(locale, "forgot-password");
@@ -62,12 +54,7 @@ public class ForgotPasswordLayoutUtilityPageEntryViewRenderer
 		requestDispatcher.include(httpServletRequest, httpServletResponse);
 	}
 
-	@Reference
-	private Language _language;
-
-	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.layout.utility.page.forgot.password)"
-	)
-	private ServletContext _servletContext;
+	private final Language _language;
+	private final ServletContext _servletContext;
 
 }
