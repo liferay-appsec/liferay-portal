7576c538e60c0ec7c806191167ad4a9d150d743d
alvarosaugarlr <alvaro.saugar@liferay.com>
2024-03-19 02:36:13 +0100

    LPD-6871 create FP Utility Page

--- portal-kernel/src/com/liferay/layout/utility/page/kernel/constants/LayoutUtilityPageEntryConstants.java
+++ portal-kernel/src/com/liferay/layout/utility/page/kernel/constants/LayoutUtilityPageEntryConstants.java
@@ -12,6 +12,8 @@ public class LayoutUtilityPageEntryConstants {
 
 	public static final String TYPE_LOGIN = "LOGIN";
 
+	public static final String TYPE_FORGOT_PASSWORD = "FORGOT_PASSWORD";
+
 	public static final String TYPE_SC_INTERNAL_SERVER_ERROR =
 		"SC_INTERNAL_SERVER_ERROR";
 
