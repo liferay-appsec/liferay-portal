9d7067f982adb23df13e2f6465ea7d7759d551f8
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2024-03-21 14:19:11 +0000

    LPD-6871 Add option to create new Forgot Pass Utility Page

---
+++ modules/apps/layout/layout-utility-page/layout-utility-page-forgot-password/src/main/java/com/liferay/layout/utility/page/forgot/password/internal/ForgotPasswordLayoutUtilityPageEntryViewRenderer.java
@@ -0,0 +1,53 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.layout.utility.page.forgot.password.internal;
+
+import com.liferay.layout.utility.page.kernel.LayoutUtilityPageEntryViewRenderer;
+import com.liferay.layout.utility.page.kernel.constants.LayoutUtilityPageEntryConstants;
+import com.liferay.portal.kernel.language.Language;
+
+import java.io.IOException;
+
+import java.util.Locale;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alvaro Saugar
+ */
+@Component(
+	property = "utility.page.type=" + LayoutUtilityPageEntryConstants.TYPE_FORGOT_PASSWORD,
+	service = LayoutUtilityPageEntryViewRenderer.class
+)
+public class ForgotPasswordLayoutUtilityPageEntryViewRenderer
+	implements LayoutUtilityPageEntryViewRenderer {
+
+	@Override
+	public String getLabel(Locale locale) {
+		return _language.get(locale, "forgot-password");
+	}
+
+	@Override
+	public String getType() {
+		return LayoutUtilityPageEntryConstants.TYPE_FORGOT_PASSWORD;
+	}
+
+	@Override
+	public void renderHTML(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException, ServletException {
+	}
+
+	@Reference
+	private Language _language;
+
+}
