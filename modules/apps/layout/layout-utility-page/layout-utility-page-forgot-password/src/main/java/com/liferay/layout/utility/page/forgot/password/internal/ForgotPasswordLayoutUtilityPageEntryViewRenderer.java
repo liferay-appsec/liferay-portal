5e5dbd8c7d7045d87d2ac2e8bd8775bb23ca9d0c
alvarosaugarlr <alvaro.saugar@liferay.com>
2024-04-04 01:55:22 +0200

    LPD-6871 - adding jsp to the UP

--- modules/apps/layout/layout-utility-page/layout-utility-page-forgot-password/src/main/java/com/liferay/layout/utility/page/forgot/password/internal/ForgotPasswordLayoutUtilityPageEntryViewRenderer.java
+++ modules/apps/layout/layout-utility-page/layout-utility-page-forgot-password/src/main/java/com/liferay/layout/utility/page/forgot/password/internal/ForgotPasswordLayoutUtilityPageEntryViewRenderer.java
@@ -10,10 +10,17 @@ import com.liferay.layout.utility.page.kernel.constants.LayoutUtilityPageEntryCo
 import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.language.Language;
 
+import java.io.IOException;
 import java.util.Locale;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Alvaro Saugar
@@ -40,7 +47,24 @@ public class ForgotPasswordLayoutUtilityPageEntryViewRenderer
 		return FeatureFlagManagerUtil.isEnabled("LPD-6378");
 	}
 
+	@Override
+	public void renderHTML(
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse)
+		throws IOException, ServletException {
+
+		RequestDispatcher requestDispatcher =
+			_servletContext.getRequestDispatcher("/forgot_password.jsp");
+
+		requestDispatcher.include(httpServletRequest, httpServletResponse);
+	}
 	@Reference
 	private Language _language;
+
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.layout.utility.page.forgot.password)"
+	)
+	private ServletContext _servletContext;
 
 }
