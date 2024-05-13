b2486b6ff103c0c9ef80c5b70b71328c7b1c6578
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2024-05-13 16:06:57 +0100

    LPD-23110 I think we can keep the old name because both checks are related to the "Lockout" policy section

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -1769,7 +1769,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 			return;
 		}
 
-		doCheckFailureCountAndLockout(user, user.getPasswordPolicy());
+		doCheckLockout(user, user.getPasswordPolicy());
 	}
 
 	/**
@@ -6202,7 +6202,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		return searchContext;
 	}
 
-	protected User doCheckFailureCountAndLockout(
+	protected User doCheckLockout(
 			User user, PasswordPolicy passwordPolicy)
 		throws PortalException {
 
@@ -7362,7 +7362,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		if (!LDAPSettingsUtil.isPasswordPolicyEnabled(user.getCompanyId())) {
 			PasswordPolicy passwordPolicy = user.getPasswordPolicy();
 
-			user = doCheckFailureCountAndLockout(user, passwordPolicy);
+			user = doCheckLockout(user, passwordPolicy);
 
 			if (!PasswordModificationThreadLocal.isPasswordModified()) {
 				user = doCheckPasswordExpired(user, passwordPolicy);
