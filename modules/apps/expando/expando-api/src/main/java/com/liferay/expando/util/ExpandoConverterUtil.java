563a2c5e0322876502f639569cc319443c932416
ChrisKian <christopher.kian@liferay.com>
2024-05-16 16:23:13 -0700

    LPD-23115 When importing an LDAP attribute to a localizable String custom field, we need to properly convert the attribute to avoid ClassCastExceptions

--- modules/apps/expando/expando-api/src/main/java/com/liferay/expando/util/ExpandoConverterUtil.java
+++ modules/apps/expando/expando-api/src/main/java/com/liferay/expando/util/ExpandoConverterUtil.java
@@ -10,6 +10,8 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.DateUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -149,6 +151,11 @@ public class ExpandoConverterUtil {
 		}
 		else if (type == ExpandoColumnConstants.STRING) {
 			return attribute[0];
+		}
+		else if (type == ExpandoColumnConstants.STRING_LOCALIZED) {
+			return new HashMapBuilder<>().put(
+				LocaleUtil.getDefault(), attribute[0]
+			).build();
 		}
 
 		return attribute;
