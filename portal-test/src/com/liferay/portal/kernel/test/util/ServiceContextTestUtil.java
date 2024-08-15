626238294ecd92a6317dea49eda544cba1d5ca94
Brian Chan <brian.chan@liferay.com>
2024-08-15 19:00:14 -0300

    LPD-33399 SF

--- portal-test/src/com/liferay/portal/kernel/test/util/ServiceContextTestUtil.java
+++ portal-test/src/com/liferay/portal/kernel/test/util/ServiceContextTestUtil.java
@@ -6,6 +6,7 @@
 package com.liferay.portal.kernel.test.util;
 
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
@@ -69,12 +70,11 @@ public class ServiceContextTestUtil {
 		serviceContext.setAssetCategoryIds(assetCategoryIds);
 		serviceContext.setAssetTagNames(assetTagNames);
 		serviceContext.setCompanyId(companyId);
-		serviceContext.setPortalURL(
-			CompanyLocalServiceUtil.getCompany(
-				companyId
-			).getPortalURL(
-				groupId
-			));
+
+		Company company = CompanyLocalServiceUtil.getCompany(companyId);
+
+		serviceContext.setPortalURL(company.getPortalURL(groupId));
+
 		serviceContext.setScopeGroupId(groupId);
 		serviceContext.setUserId(userId);
 
