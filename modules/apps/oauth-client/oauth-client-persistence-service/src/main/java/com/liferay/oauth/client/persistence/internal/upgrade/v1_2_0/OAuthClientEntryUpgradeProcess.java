9e3776fa39493490acf075ff46c8c993b1ef7900
Istvan Sajtos <istvan.sajtos@liferay.com>
2024-09-20 18:39:26 +0200

    LPD-34901 Add upgrade process

---
+++ modules/apps/oauth-client/oauth-client-persistence-service/src/main/java/com/liferay/oauth/client/persistence/internal/upgrade/v1_2_0/OAuthClientEntryUpgradeProcess.java
@@ -0,0 +1,67 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.oauth.client.persistence.internal.upgrade.v1_2_0;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import java.sql.PreparedStatement;
+
+import java.util.Dictionary;
+
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+/**
+ * @author Istvan Sajtos
+ */
+public class OAuthClientEntryUpgradeProcess extends UpgradeProcess {
+
+	public OAuthClientEntryUpgradeProcess(
+		ConfigurationAdmin configurationAdmin) {
+
+		_configurationAdmin = configurationAdmin;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		Configuration[] configurations = _configurationAdmin.listConfigurations(
+			"(service.factoryPid=com.liferay.portal.security.sso.openid." +
+				"connect.internal.configuration." +
+					"OpenIdConnectProviderConfiguration)");
+
+		if (configurations != null) {
+			for (Configuration configuration : configurations) {
+				Dictionary<String, Object> properties =
+					configuration.getProperties();
+
+				if (properties != null) {
+					String openIdConnectClientId = GetterUtil.getString(
+						properties.get("openIdConnectClientId"));
+
+					long discoveryEndPointCacheInMillis = GetterUtil.getLong(
+						properties.get("discoveryEndPointCacheInMillis"));
+
+					try (PreparedStatement preparedStatement =
+							connection.prepareStatement(
+								"update OAuthClientEntry set " +
+									"metadataCacheInMillis = ? where " +
+										"clientId = ?")) {
+
+						preparedStatement.setLong(
+							1, discoveryEndPointCacheInMillis);
+						preparedStatement.setString(2, openIdConnectClientId);
+
+						preparedStatement.execute();
+					}
+				}
+			}
+		}
+	}
+
+	private final ConfigurationAdmin _configurationAdmin;
+
+}
