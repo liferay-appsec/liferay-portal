8891b2d405e8dc97fd9f9a89e81f14cc197c9de1
Jorge García Jiménez <jorge.garcia@liferay.com>
2024-09-25 16:03:24 +0200

    LPD-35832 add unit test

--- modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/test/java/com/liferay/portal/security/sso/openid/connect/internal/OpenIdConnectAuthenticationHandlerImplTest.java
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/test/java/com/liferay/portal/security/sso/openid/connect/internal/OpenIdConnectAuthenticationHandlerImplTest.java
@@ -6,12 +6,22 @@
 package com.liferay.portal.security.sso.openid.connect.internal;
 
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.security.sso.openid.connect.internal.util.OpenIdConnectRequestParametersUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import com.nimbusds.jwt.JWT;
 import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.langtag.LangTag;
 
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
@@ -19,6 +29,8 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import org.mockito.Mockito;
+
+import org.springframework.mock.web.MockHttpServletRequest;
 
 /**
  * @author Tamas Biro
@@ -51,6 +63,72 @@ public class OpenIdConnectAuthenticationHandlerImplTest {
 			).toString());
 
 		Assert.assertNull(claims.get("email"));
+	}
+
+	@Test
+	public void testUILocaleLangTag() {
+		List<LangTag> langTags = _getLangTags(
+			OpenIdConnectRequestParametersUtil.UILocaleMode.DISABLED);
+
+		Assert.assertNull(langTags);
+
+		langTags = _getLangTags(
+			OpenIdConnectRequestParametersUtil.UILocaleMode.
+				LOWERCASE_BCP47_LANGUAGE_CODE);
+
+		Assert.assertEquals(
+			"en-us",
+			langTags.get(
+				0
+			).toString());
+
+		langTags = _getLangTags(
+			OpenIdConnectRequestParametersUtil.UILocaleMode.DEFAULT);
+
+		Assert.assertEquals(
+			"en-US",
+			langTags.get(
+				0
+			).toString());
+	}
+
+	protected Language language = Mockito.mock(Language.class);
+	protected Portal portal = Mockito.mock(Portal.class);
+
+	private List<LangTag> _getLangTags(
+		OpenIdConnectRequestParametersUtil.UILocaleMode uiLocaleMode) {
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		Mockito.doReturn(
+			LocaleUtil.US
+		).when(
+			portal
+		).getLocale(
+			Mockito.any(HttpServletRequest.class)
+		);
+
+		Mockito.doReturn(
+			"en-US"
+		).when(
+			language
+		).getBCP47LangTag(
+			Mockito.any(Locale.class)
+		);
+
+		OpenIdConnectAuthenticationHandlerImpl
+			openIdConnectAuthenticationHandlerImpl =
+				new OpenIdConnectAuthenticationHandlerImpl();
+
+		ReflectionTestUtil.setFieldValue(
+			openIdConnectAuthenticationHandlerImpl, "_language", language);
+
+		ReflectionTestUtil.setFieldValue(
+			openIdConnectAuthenticationHandlerImpl, "_portal", portal);
+
+		return openIdConnectAuthenticationHandlerImpl.getLangTags(
+			mockHttpServletRequest, uiLocaleMode);
 	}
 
 	private Map<String, Object> _getUserInfoClaims(String claimSetJSON)
