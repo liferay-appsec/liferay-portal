e95a82f32d06538e57d981ab250518c45f012a3b
Istvan Sajtos <istvan.sajtos@liferay.com>
2024-10-22 23:49:07 +0200

    LPD-34901 Add test for metadata caching

---
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/test/java/com/liferay/portal/security/sso/openid/connect/internal/AuthorizationServerMetadataResolverTest.java
@@ -0,0 +1,70 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.security.sso.openid.connect.internal;
+
+import com.liferay.portal.cache.test.util.TestPortalCache;
+import com.liferay.portal.kernel.cache.PortalCache;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import com.nimbusds.openid.connect.sdk.op.OIDCProviderMetadata;
+
+import java.lang.reflect.Field;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Istvan Sajtos
+ */
+public class AuthorizationServerMetadataResolverTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Before
+	public void setUp() throws Exception {
+		Field field =
+			AuthorizationServerMetadataResolver.class.getDeclaredField(
+				"_oidcProviderMetadataPortalCache");
+
+		field.setAccessible(true);
+		field.set(_authorizationServerMetadataResolver, _portalCache);
+	}
+
+	@Test
+	public void testMetadataIsCached() throws Exception {
+		String authServerWellKnownURI =
+			"https://accounts.google.com/.well-known/openid-configuration";
+
+		String clientId = RandomTestUtil.randomString();
+
+		int metadataCacheInSecs = 90;
+
+		OIDCProviderMetadata oidcProviderMetadata1 =
+			_authorizationServerMetadataResolver.resolveOIDCProviderMetadata(
+				authServerWellKnownURI, clientId, metadataCacheInSecs);
+
+		OIDCProviderMetadata oidcProviderMetadata2 =
+			_authorizationServerMetadataResolver.resolveOIDCProviderMetadata(
+				authServerWellKnownURI, clientId, metadataCacheInSecs);
+
+		Assert.assertTrue(oidcProviderMetadata1 == oidcProviderMetadata2);
+	}
+
+	private final AuthorizationServerMetadataResolver
+		_authorizationServerMetadataResolver =
+			new AuthorizationServerMetadataResolver();
+	private final PortalCache<String, OIDCProviderMetadata> _portalCache =
+		new TestPortalCache<>(
+			AuthorizationServerMetadataResolverTest.class.getName());
+
+}
