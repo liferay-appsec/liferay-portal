58b552b4c24e2d6d3d64056eec746d9ef4f3aba3
ChrisKian <christopher.kian@liferay.com>
2024-10-31 14:23:51 -0700

    LPD-36309 Remove need for getDefaultPasswordPolicy method to check LDAPSettingsUtil.isPasswordPolicyEnabled(companyId).  If we enter getDefaultPasswordPolicy() method, we should return a policy, regardless of LDAP settings.

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -1293,7 +1293,16 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		}
 
 		user.setPasswordEncrypted(true);
-		user.setPasswordReset(_isPasswordReset(companyId));
+
+		if ((ldapServerId <= 0) ||
+			!LDAPSettingsUtil.isPasswordPolicyEnabled(companyId)) {
+
+			user.setPasswordReset(_isPasswordReset(companyId));
+		}
+		else {
+			user.setPasswordReset(false);
+		}
+
 		user.setScreenName(screenName);
 		user.setEmailAddress(emailAddress);
 		user.setLdapServerId(ldapServerId);
@@ -7466,7 +7475,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 			PasswordPolicy passwordPolicy =
 				_passwordPolicyLocalService.getDefaultPasswordPolicy(companyId);
 
-			if ((passwordPolicy != null) && passwordPolicy.isChangeable() &&
+			if (passwordPolicy.isChangeable() &&
 				passwordPolicy.isChangeRequired()) {
 
 				return true;
