76631a56aec1e1b01620d46525069c5dd4c11b3f
alvarosaugarlr <alvaro.saugar@liferay.com>
2024-11-28 12:32:12 +0100

    LPS-206534 / LPD-4254 - test to check the behavior when the property of admin user is set to blank

--- modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceTest.java
+++ modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceTest.java
@@ -39,6 +39,7 @@ import com.liferay.portal.kernel.exception.NoSuchVirtualHostException;
 import com.liferay.portal.kernel.exception.RequiredCompanyException;
 import com.liferay.portal.kernel.instance.PortalInstancePool;
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.BaseModelListener;
@@ -104,8 +105,14 @@ import com.liferay.portal.util.PropsValues;
 import com.liferay.site.model.adapter.StagedGroup;
 import com.liferay.sites.kernel.util.Sites;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -190,6 +197,68 @@ public class CompanyLocalServiceTest {
 
 		for (String webId : PortalInstancePool.getWebIds()) {
 			Assert.assertNotEquals(company.getWebId(), webId);
+		}
+	}
+
+	@Test
+	public void testAddAndDeleteCompanyWithAdminDefaultPasswordBlank()
+		throws Exception {
+
+		Company company = null;
+		Boolean findText1 = false;
+		Boolean findText2 = false;
+		String languageKey =
+			"remove-this-message-delete-default-admin-password-property-value";
+
+		try (SafeCloseable safeCloseable1 =
+				PropsValuesTestUtil.swapWithSafeCloseable(
+					"DEFAULT_ADMIN_PASSWORD", null);
+			SafeCloseable safeCloseable2 =
+				PropsValuesTestUtil.swapWithSafeCloseable(
+					"TERMS_OF_USE_REQUIRED", false)) {
+
+			Locale defaultLocale = LocaleUtil.getDefault(); //getSiteDefault();
+
+			String searchText1 = LanguageUtil.get(defaultLocale, languageKey);
+			String searchText2 = LanguageUtil.get(
+				defaultLocale, "set-password");
+
+			String virtualHostName = "www.able.com";
+
+			company = addCompany(virtualHostName);
+
+			URL url = new URL("http://www.able.com:8080");
+
+			HttpURLConnection connection =
+				(HttpURLConnection)url.openConnection();
+
+			connection.setRequestMethod("GET");
+
+			BufferedReader reader = new BufferedReader(
+				new InputStreamReader(connection.getInputStream()));
+
+			StringBuilder response = new StringBuilder();
+			String line;
+
+			while (((line = reader.readLine()) != null) && !findText1) {
+				findText1 = line.contains(searchText1);
+				response.append(line);
+			}
+
+			reader.close();
+
+			findText2 = response.toString(
+			).contains(
+				searchText2
+			);
+
+			Assert.assertFalse(findText1);
+			Assert.assertTrue(findText2);
+		}
+		finally {
+			if (company != null) {
+				_companyLocalService.deleteCompany(company);
+			}
 		}
 	}
 
