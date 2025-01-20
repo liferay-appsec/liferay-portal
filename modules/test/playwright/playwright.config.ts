29201d0fae1c572e87401716b0528b7085878497
alvarosaugarlr <alvaro.saugar@liferay.com>
2025-01-21 00:54:05 +0100

    LPS-206534 / LPD-4254 add test

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -100,6 +100,7 @@ import {config as scimConfiguraitonWebConfig} from './tests/scim-configuration-w
 import {config as searchExperiencesWebConfig} from './tests/search-experiences-web/config';
 import {config as segmentExperimentWebConfig} from './tests/segment-experiment-web/config';
 import {config as segmentsWebConfig} from './tests/segments-web/config';
+import {config as setAdminPassword} from './tests/set-admin-password/config';
 import {
 	pageManagementSiteSetup,
 	pageManagementSiteTeardown,
@@ -225,6 +226,7 @@ export default defineConfig({
 		searchExperiencesWebConfig,
 		segmentExperimentWebConfig,
 		segmentsWebConfig,
+		setAdminPassword,
 		siteAdminWebConfig,
 		siteNavigationAdminWebConfig,
 		siteNavigationBreadcrumbWebConfig,
