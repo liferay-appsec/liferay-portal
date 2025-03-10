cdbab589139284a9b1462bbc1e792d0a8cac622d
Rafael Praxedes <rafael.praxedes@liferay.com>
2025-03-10 16:04:49 -0300

    LPD-48901 Inline the current SchemaResourceManagerImpl into the SchemaResourceImpl since we are not reusing the inherited methods

--- modules/dxp/apps/scim/scim-rest-impl/src/main/java/com/liferay/scim/rest/internal/resource/v1_0/SchemaResourceImpl.java
+++ modules/dxp/apps/scim/scim-rest-impl/src/main/java/com/liferay/scim/rest/internal/resource/v1_0/SchemaResourceImpl.java
@@ -8,32 +8,54 @@ package com.liferay.scim.rest.internal.resource.v1_0;
 import com.liferay.expando.kernel.service.ExpandoColumnLocalService;
 import com.liferay.expando.kernel.service.ExpandoTableLocalService;
 import com.liferay.expando.kernel.service.ExpandoValueLocalService;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.UserGroupLocalService;
 import com.liferay.portal.kernel.service.UserGroupService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.service.UserService;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.URLUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
 import com.liferay.portal.search.searcher.Searcher;
-import com.liferay.scim.rest.internal.manager.SchemaResourceManagerImpl;
 import com.liferay.scim.rest.internal.manager.UserManagerImpl;
+import com.liferay.scim.rest.internal.util.ScimUtil;
 import com.liferay.scim.rest.resource.v1_0.SchemaResource;
+
+import java.io.IOException;
+
+import java.net.URL;
 
 import java.util.Map;
 
 import javax.ws.rs.core.Response;
 
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
+import org.wso2.charon3.core.exceptions.AbstractCharonException;
+import org.wso2.charon3.core.exceptions.ConflictException;
+import org.wso2.charon3.core.exceptions.InternalErrorException;
+import org.wso2.charon3.core.exceptions.NotFoundException;
 import org.wso2.charon3.core.extensions.UserManager;
+import org.wso2.charon3.core.protocol.ResponseCodeConstants;
 import org.wso2.charon3.core.protocol.SCIMResponse;
-import org.wso2.charon3.core.protocol.endpoints.SchemaResourceManager;
+import org.wso2.charon3.core.protocol.endpoints.AbstractResourceManager;
+import org.wso2.charon3.core.schema.SCIMConstants;
 
 /**
  * @author Alvaro Saugar
@@ -46,14 +68,12 @@ public class SchemaResourceImpl extends BaseSchemaResourceImpl {
 
 	@Override
 	public Object getV2SchemaById(String id) throws Exception {
-		return _buildResponse(
-			_schemaResourceManager.get(id, _userManager, null, null));
+		return _buildResponse(_getSCIMResponse(id, _userManager));
 	}
 
 	@Override
 	public Object getV2Schemas() throws Exception {
-		return _buildResponse(
-			_schemaResourceManager.get(null, _userManager, null, null));
+		return getV2SchemaById(null);
 	}
 
 	@Activate
@@ -85,8 +105,157 @@ public class SchemaResourceImpl extends BaseSchemaResourceImpl {
 		return responseBuilder.build();
 	}
 
-	private static final SchemaResourceManager _schemaResourceManager =
-		new SchemaResourceManagerImpl();
+	private JSONObject _createSchema(String attribute, String jsonFile)
+		throws IOException, JSONException {
+
+		Bundle bundle = FrameworkUtil.getBundle(SchemaResourceImpl.class);
+
+		URL userSchemaURL = bundle.getResource(
+			"META-INF/schemas/json/" + jsonFile);
+
+		JSONObject jsonObject = _jsonFactory.createJSONObject(
+			URLUtil.toString(userSchemaURL));
+
+		JSONObject metaJSONObject = jsonObject.getJSONObject("meta");
+
+		String locationString = metaJSONObject.getString("location");
+
+		metaJSONObject.put("location", attribute + locationString);
+
+		JSONArray schemasJSONArray = JSONUtil.put(
+			"urn:ietf:params:scim:schemas:core:2.0:Schema");
+
+		jsonObject.put("schemas", schemasJSONArray);
+
+		return jsonObject;
+	}
+
+	private Map<String, String> _getResponseHeaders() throws NotFoundException {
+		return HashMapBuilder.put(
+			SCIMConstants.CONTENT_TYPE_HEADER, SCIMConstants.APPLICATION_JSON
+		).put(
+			SCIMConstants.LOCATION_HEADER,
+			AbstractResourceManager.getResourceEndpointURL(
+				SCIMConstants.SCHEMAS_ENDPOINT)
+		).build();
+	}
+
+	private String _getSchema(String attribute, String id)
+		throws AbstractCharonException {
+
+		try {
+			Map<String, String> schemaIdJsonFileNameStringMap =
+				ScimUtil.getMapSchemaIdJsonFileName();
+
+			if (Validator.isNotNull(schemaIdJsonFileNameStringMap.get(id))) {
+				JSONObject schemajsonObject = _createSchema(
+					attribute, schemaIdJsonFileNameStringMap.get(id));
+
+				if (schemajsonObject != null) {
+					return schemajsonObject.toString();
+				}
+			}
+
+			throw new NotFoundException("No schema found with schema ID " + id);
+		}
+		catch (IOException | JSONException exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception);
+			}
+
+			String error = "Error getting the schemas.";
+
+			throw new InternalErrorException(error);
+		}
+	}
+
+	private String _getSchemas(String attribute)
+		throws AbstractCharonException {
+
+		try {
+			Map<String, String> schemaIdJsonFileNameStringMap =
+				ScimUtil.getMapSchemaIdJsonFileName();
+
+			JSONObject rootJSONObject = JSONUtil.put(
+				"itemsPerPage", 3
+			).put(
+				"schemas",
+				_jsonFactory.createJSONArray(
+					"[\"urn:ietf:params:scim:api:messages:2.0:ListResponse\"]")
+			).put(
+				"startIndex", 1
+			).put(
+				"totalResults", schemaIdJsonFileNameStringMap.size()
+			);
+
+			JSONArray resourcesJSONArray = _jsonFactory.createJSONArray();
+
+			for (Map.Entry<String, String> entry :
+					schemaIdJsonFileNameStringMap.entrySet()) {
+
+				resourcesJSONArray.put(
+					_createSchema(attribute, entry.getValue()));
+			}
+
+			rootJSONObject.put("Resources", resourcesJSONArray);
+
+			return rootJSONObject.toString();
+		}
+		catch (IOException | JSONException exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception);
+			}
+
+			String error = "Error getting the schemas.";
+
+			throw new InternalErrorException(error);
+		}
+	}
+
+	private SCIMResponse _getSCIMResponse(String id, UserManager userManager) {
+		String responseString = null;
+
+		try {
+			if (userManager != null) {
+				userManager.getCoreSchema();
+
+				if (Validator.isNull(id)) {
+					responseString = _getSchemas(null);
+				}
+				else {
+					responseString = _getSchema(null, id);
+				}
+
+				if (Validator.isNull(responseString)) {
+					throw new NotFoundException(
+						"No schema found with schema ID " + id);
+				}
+
+				return new SCIMResponse(
+					ResponseCodeConstants.CODE_OK, responseString,
+					_getResponseHeaders());
+			}
+
+			String error = "Provided user manager handler is null.";
+
+			throw new InternalErrorException(error);
+		}
+		catch (AbstractCharonException abstractCharonException) {
+			return AbstractResourceManager.encodeSCIMException(
+				abstractCharonException);
+		}
+		catch (Exception exception) {
+			if (exception instanceof ConflictException) {
+				return AbstractResourceManager.encodeSCIMException(
+					(ConflictException)exception);
+			}
+
+			throw exception;
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SchemaResourceImpl.class);
 
 	@Reference
 	private ClassNameLocalService _classNameLocalService;
@@ -105,6 +274,9 @@ public class SchemaResourceImpl extends BaseSchemaResourceImpl {
 
 	@Reference
 	private ExpandoValueLocalService _expandoValueLocalService;
+
+	@Reference
+	private JSONFactory _jsonFactory;
 
 	@Reference
 	private Searcher _searcher;
