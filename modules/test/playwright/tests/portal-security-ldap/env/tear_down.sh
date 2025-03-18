d83af74bff850637c70d5089c4064264cf122ae2
ChrisKian <christopher.kian@liferay.com>
2025-03-18 11:35:54 -0700

    LPD-47428 SF, Inline single-use variables

--- modules/test/playwright/tests/portal-security-ldap/env/tear_down.sh
+++ modules/test/playwright/tests/portal-security-ldap/env/tear_down.sh
@@ -13,13 +13,9 @@ function main {
 }
 
 function ldap_tear_down {
-	local removeGroupsLdif="${CURRENT_DIR_NAME}/removeGroups.ldif"
+	ldapdelete -cx -D "cn=admin,dc=example,dc=com" -w "secret" -f ${CURRENT_DIR_NAME}/removeGroups.ldif
 
-	ldapdelete -cx -D "cn=admin,dc=example,dc=com" -w "secret" -f ${removeGroupsLdif}
-
-	local removeUsersLdif="${CURRENT_DIR_NAME}/removeUsers.ldif"
-
-	ldapdelete -cx -D "cn=admin,dc=example,dc=com" -w "secret" -f ${removeUsersLdif}
+	ldapdelete -cx -D "cn=admin,dc=example,dc=com" -w "secret" -f ${CURRENT_DIR_NAME}/removeUsers.ldif
 
 	kill -INT `cat /usr/local/var/run/slapd.pid`
 }
