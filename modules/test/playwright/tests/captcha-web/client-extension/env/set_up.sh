a7acfcfbf8d4012a212bd0d9ca4b174becaaa355
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2025-05-15 13:17:58 -0300

    LPD-44395 SF Rename

--- modules/test/playwright/tests/captcha-web/client-extension/env/set_up.sh
+++ modules/test/playwright/tests/captcha-web/client-extension/env/set_up.sh
@@ -9,7 +9,7 @@ source ${CURRENT_DIR_NAME}/../../../../env/common.sh
 function main {
 	default_set_up
 
-	start_client_extension_spring_boot_application workspaces/liferay-sample-workspace/client-extensions/liferay-sample-captcha
+	start_client_extension_spring_boot_application workspaces/liferay-sample-workspace/client-extensions/liferay-sample-etc-captcha
 }
 
 main "${@}"
