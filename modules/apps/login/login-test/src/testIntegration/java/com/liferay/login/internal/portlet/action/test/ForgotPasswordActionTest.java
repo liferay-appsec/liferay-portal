5564fb3649a7c76db3c07cf1076689444f10859e
alvarosaugarlr <alvaro.saugar@liferay.com>
2025-06-02 02:08:34 +0200

    LPD-6871 - Add integration tests

---
+++ modules/apps/login/login-test/src/testIntegration/java/com/liferay/login/internal/portlet/action/test/ForgotPasswordActionTest.java
@@ -0,0 +1,340 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.login.internal.portlet.action.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.fragment.constants.FragmentConstants;
+import com.liferay.fragment.model.FragmentCollection;
+import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentCollectionService;
+import com.liferay.fragment.service.FragmentEntryLinkService;
+import com.liferay.fragment.service.FragmentEntryService;
+import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
+import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
+import com.liferay.layout.util.structure.ContainerStyledLayoutStructureItem;
+import com.liferay.layout.util.structure.LayoutStructure;
+import com.liferay.layout.utility.page.kernel.constants.LayoutUtilityPageEntryConstants;
+import com.liferay.layout.utility.page.model.LayoutUtilityPageEntry;
+import com.liferay.layout.utility.page.service.LayoutUtilityPageEntryLocalService;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.Ticket;
+import com.liferay.portal.kernel.model.TicketConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.security.pwd.PasswordEncryptorUtil;
+import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.service.TicketLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.test.rule.FeatureFlag;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.segments.service.SegmentsExperienceLocalService;
+import com.liferay.site.initializer.SiteInitializerRegistry;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Alvaro Saugar
+ */
+@FeatureFlag("LPD-6378")
+@RunWith(Arquillian.class)
+public class ForgotPasswordActionTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_company = CompanyLocalServiceUtil.getCompany(_group.getCompanyId());
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group.getGroupId());
+
+		ServiceContextThreadLocal.pushServiceContext(_serviceContext);
+
+		_group = GroupTestUtil.addGroup();
+
+		_company = CompanyLocalServiceUtil.getCompany(_group.getCompanyId());
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group.getGroupId());
+
+		ServiceContextThreadLocal.pushServiceContext(_serviceContext);
+
+		_layoutUtilityPageEntry =
+			_layoutUtilityPageEntryLocalService.addLayoutUtilityPageEntry(
+				null, _serviceContext.getUserId(), _group.getGroupId(), 0, 0,
+				true, RandomTestUtil.randomString(),
+				LayoutUtilityPageEntryConstants.TYPE_FORGOT_PASSWORD, 0,
+				_serviceContext);
+
+		_user = UserTestUtil.addGroupUser(_group, RoleConstants.POWER_USER);
+
+		_layout = _layoutLocalService.fetchLayout(
+			_layoutUtilityPageEntry.getPlid());
+
+		long defaultSegmentsExperienceId =
+			_segmentsExperienceLocalService.fetchDefaultSegmentsExperienceId(
+				_layout.getPlid());
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		FragmentCollection fragmentCollection =
+			_fragmentCollectionService.addFragmentCollection(
+				null, _group.getGroupId(), "Fragment Collection",
+				StringPool.BLANK, serviceContext);
+
+		_fragmentText = RandomTestUtil.randomString();
+
+		FragmentEntry fragmentEntry = _fragmentEntryService.addFragmentEntry(
+			null, _group.getGroupId(),
+			fragmentCollection.getFragmentCollectionId(), "fragment-entry",
+			"Fragment Entry", null, "<div>" + _fragmentText + "</div>", null,
+			false, null, null, 0, false, false, FragmentConstants.TYPE_SECTION,
+			null, WorkflowConstants.STATUS_APPROVED, serviceContext);
+
+		FragmentEntryLink fragmentEntryLink =
+			_fragmentEntryLinkService.addFragmentEntryLink(
+				null, _group.getGroupId(), 0,
+				fragmentEntry.getFragmentEntryId(), defaultSegmentsExperienceId,
+				_layout.getPlid(), StringPool.BLANK, fragmentEntry.getHtml(),
+				StringPool.BLANK, "{fieldSets: []}", StringPool.BLANK,
+				StringPool.BLANK, 0, null, fragmentEntry.getType(),
+				ServiceContextTestUtil.getServiceContext(
+					_group, _user.getUserId()));
+
+		LayoutPageTemplateStructure layoutPageTemplateStructure =
+			_layoutPageTemplateStructureLocalService.
+				fetchLayoutPageTemplateStructure(
+					_layout.getGroupId(), _layout.getPlid());
+
+		LayoutStructure layoutStructure = LayoutStructure.of(
+			layoutPageTemplateStructure.getDefaultSegmentsExperienceData());
+
+		ContainerStyledLayoutStructureItem containerStyledLayoutStructureItem =
+			(ContainerStyledLayoutStructureItem)
+				layoutStructure.addContainerStyledLayoutStructureItem(
+					layoutStructure.getMainItemId(), 0);
+
+		containerStyledLayoutStructureItem.setWidthType("fixed");
+
+		layoutStructure.addFragmentStyledLayoutStructureItem(
+			fragmentEntryLink.getFragmentEntryLinkId(),
+			containerStyledLayoutStructureItem.getItemId(), 0);
+
+		JSONObject dataJSONObject = layoutStructure.toJSONObject();
+
+		_layoutPageTemplateStructureLocalService.
+			updateLayoutPageTemplateStructureData(
+				_group.getGroupId(), _layout.getPlid(),
+				defaultSegmentsExperienceId, dataJSONObject.toString());
+
+		UserTestUtil.setUser(
+			_userLocalService.getGuestUser(TestPropsValues.getCompanyId()));
+	}
+
+	@After
+	public void tearDown() {
+		ServiceContextThreadLocal.popServiceContext();
+	}
+
+	@Test
+	public void testUpdatePasswordRedirectWithLayoutUtilityPageEntrySetASUndefault()
+		throws Exception {
+
+		_layoutUtilityPageEntry.setDefaultLayoutUtilityPageEntry(false);
+
+		_layoutUtilityPageEntry =
+			_layoutUtilityPageEntryLocalService.updateLayoutUtilityPageEntry(
+				_layoutUtilityPageEntry);
+
+		Assert.assertFalse(_fragmentIsShown(true));
+	}
+
+	@Test
+	public void testUpdatePasswordRedirectWithLayoutUtilityPageEntryWithoutPlid()
+		throws Exception {
+
+		_layoutUtilityPageEntry.setDefaultLayoutUtilityPageEntry(true);
+
+		_layoutUtilityPageEntry =
+			_layoutUtilityPageEntryLocalService.updateLayoutUtilityPageEntry(
+				_layoutUtilityPageEntry);
+
+		Assert.assertTrue(_fragmentIsShown(false));
+	}
+
+	@Test
+	public void testUpdatePasswordRedirectWithLayoutUtilityPageEntryWithPlid()
+		throws Exception {
+
+		_layoutUtilityPageEntry.setDefaultLayoutUtilityPageEntry(true);
+
+		_layoutUtilityPageEntry =
+			_layoutUtilityPageEntryLocalService.updateLayoutUtilityPageEntry(
+				_layoutUtilityPageEntry);
+
+		Assert.assertTrue(_fragmentIsShown(true));
+	}
+
+	private boolean _fragmentIsShown(boolean hasPlid) throws Exception {
+		Ticket ticket = _ticketLocalService.addDistinctTicket(
+			_user.getCompanyId(), User.class.getName(), _user.getUserId(),
+			TicketConstants.TYPE_PASSWORD, null,
+			new Date(System.currentTimeMillis() + 3600000),
+			new ServiceContext());
+
+		String ticketId = String.valueOf(ticket.getTicketId());
+		String ticketKey = ticket.getKey();
+
+		URL url;
+
+		if (hasPlid) {
+			url = new URL(
+				StringBundler.concat(
+					"http://", _company.getVirtualHostname(),
+					":8080/c/portal/update_password?p_l_id=", _layout.getPlid(),
+					"&ticketId=", ticketId, "&ticketId=", ticketKey));
+		}
+		else {
+			url = new URL(
+				StringBundler.concat(
+					"http://", _company.getVirtualHostname(),
+					":8080/c/portal/update_password?ticketId=", ticketId,
+					"&ticketId=", ticketKey));
+		}
+
+		ticket.setKey(PasswordEncryptorUtil.encrypt(ticket.getKey()));
+
+		_ticketLocalService.updateTicket(ticket);
+
+		HttpURLConnection httpURLConnection =
+			(HttpURLConnection)url.openConnection();
+
+		httpURLConnection.setRequestMethod("GET");
+
+		BufferedReader in = new BufferedReader(
+			new InputStreamReader(httpURLConnection.getInputStream()));
+
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+
+		in.close();
+
+		String res = response.toString();
+
+		return res.contains(_fragmentText);
+	}
+
+	private Company _company;
+
+	@Inject
+	private FragmentCollectionService _fragmentCollectionService;
+
+	@Inject
+	private FragmentEntryLinkService _fragmentEntryLinkService;
+
+	@Inject
+	private FragmentEntryService _fragmentEntryService;
+
+	private String _fragmentText;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private GroupLocalService _groupLocalService;
+
+	private Layout _layout;
+
+	@Inject
+	private LayoutLocalService _layoutLocalService;
+
+	@Inject
+	private LayoutPageTemplateStructureLocalService
+		_layoutPageTemplateStructureLocalService;
+
+	@DeleteAfterTestRun
+	private LayoutUtilityPageEntry _layoutUtilityPageEntry;
+
+	@Inject
+	private LayoutUtilityPageEntryLocalService
+		_layoutUtilityPageEntryLocalService;
+
+	@Inject
+	private Portal _portal;
+
+	@Inject
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
+	@Inject
+	private RoleLocalService _roleLocalService;
+
+	@Inject
+	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
+
+	private ServiceContext _serviceContext;
+
+	@Inject
+	private SiteInitializerRegistry _siteInitializerRegistry;
+
+	@Inject
+	private TicketLocalService _ticketLocalService;
+
+	private User _user;
+
+	@Inject
+	private UserLocalService _userLocalService;
+
+}
