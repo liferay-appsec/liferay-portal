b78fa43a662d8b121b40d317e65964a05d68a5de
Manuele Castro <manuele.castro@liferay.com>
2025-07-22 19:31:14 -0300

    LPD-60037 Add test

---
+++ modules/apps/sanitizer/sanitizer-test/src/testIntegration/java/com/liferay/sanitizer/test/SanitizerUtilTest.java
@@ -0,0 +1,44 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.sanitizer.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.sanitizer.SanitizerUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Manuele Castro
+ */
+@RunWith(Arquillian.class)
+public class SanitizerUtilTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testSanitize() throws Exception {
+		String sanitizedString = SanitizerUtil.sanitize(
+			TestPropsValues.getCompanyId(), TestPropsValues.getGroupId(),
+			TestPropsValues.getUserId(), User.class.getName(), 0,
+			ContentTypes.TEXT_HTML, "\" autofocus onfocus=alert(1) x=\"");
+
+		Assert.assertEquals(
+			"&quot; autofocus onfocus=alert(1) x=&quot;", sanitizedString);
+	}
+
+}
