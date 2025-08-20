0b1fd54dc5a2ae0899620852071bff705f3e13ed
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2025-08-20 12:21:34 -0300

    LPD-37323 buildREST

--- modules/dxp/apps/saml/saml-admin-rest-client/src/main/java/com/liferay/saml/admin/rest/client/dto/v1_0/SamlProvider.java
+++ modules/dxp/apps/saml/saml-admin-rest-client/src/main/java/com/liferay/saml/admin/rest/client/dto/v1_0/SamlProvider.java
@@ -232,7 +232,7 @@ public class SamlProvider implements Cloneable, Serializable {
 
 	public static enum Role {
 
-		IDP("idp"), MULTIROLE("multirole"), SP("sp");
+		IB("ib"), IDP("idp"), SP("sp");
 
 		public static Role create(String value) {
 			for (Role role : values()) {
