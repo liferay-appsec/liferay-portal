075c9b45d2b69c7c663e48629b4f2dadb3e20800
Caio Farias <caio.farias@liferay.com>
2025-08-25 16:59:11 -0300

    LPD-51345 add changes to object definition thread local

--- modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionThreadLocal.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionThreadLocal.java
@@ -6,13 +6,64 @@
 package com.liferay.object.definition.util;
 
 import com.liferay.object.entry.util.ObjectEntryThreadLocal;
+import com.liferay.object.exception.ObjectEntryValidationException;
+import com.liferay.object.model.ObjectField;
 import com.liferay.petra.lang.CentralizedThreadLocal;
 import com.liferay.petra.lang.SafeCloseable;
+
+import java.util.List;
 
 /**
  * @author Shuyang Zhou
  */
 public class ObjectDefinitionThreadLocal {
+
+	public static String getObjectDefinitionERC() {
+		return _objectDefinitionERC.get();
+	}
+
+	public static List<ObjectField> getObjectDefinitionObjectFields() {
+		return _objectDefinitionObjectFields.get();
+	}
+
+	public static List<ObjectEntryValidationException.ValidationError>
+		getValidationErrors() {
+
+		return _validationErrors.get();
+	}
+
+	public static void handleAsValidationError(
+		Exception exception, String key) {
+
+		ObjectEntryValidationException.ValidationError validationError =
+			new ObjectEntryValidationException.ValidationError(
+				exception.getMessage(), key,
+				exception.getClass(
+				).getName());
+
+		List<ObjectEntryValidationException.ValidationError> validationErrors =
+			_validationErrors.get();
+
+		if (validationErrors == null) {
+			return;
+		}
+
+		validationErrors.add(validationError);
+
+		_validationErrors.set(validationErrors);
+	}
+
+	public static boolean hasValidationErrors() {
+		List<ObjectEntryValidationException.ValidationError> validationErrors =
+			_validationErrors.get();
+
+		if (validationErrors == null) {
+			return false;
+		};
+
+		return !_validationErrors.get(
+		).isEmpty();
+	}
 
 	public static boolean isDeleteObjectDefinitionId(long objectDefinitionId) {
 		Long deleteObjectDefinitionId = _deleteObjectDefinitionId.get();
@@ -26,14 +77,53 @@ public class ObjectDefinitionThreadLocal {
 		return false;
 	}
 
+	public static boolean isSkipThrowException() {
+		return _skipThrowException.get();
+	}
+
 	public static SafeCloseable setDeleteObjectDefinitionIdWithSafeCloseable(
 		long id) {
 
 		return _deleteObjectDefinitionId.setWithSafeCloseable(id);
 	}
 
+	public static void setObjectDefinitionERC(String externalReferenceCode) {
+		_objectDefinitionERC.set(externalReferenceCode);
+	}
+
+	public static void setObjectDefinitionObjectFields(
+		List<ObjectField> objectDefinitionObjectFields) {
+
+		_objectDefinitionObjectFields.set(objectDefinitionObjectFields);
+	}
+
+	public static void setSkipThrowException(boolean skipThrowException) {
+		_skipThrowException.set(skipThrowException);
+	}
+
+	public static void setValidationErrors(
+		List<ObjectEntryValidationException.ValidationError> validationErrors) {
+
+		_validationErrors.set(validationErrors);
+	}
+
 	private static final CentralizedThreadLocal<Long>
 		_deleteObjectDefinitionId = new CentralizedThreadLocal<>(
 			ObjectEntryThreadLocal.class + "._deleteObjectDefinitionId");
+	private static final CentralizedThreadLocal<String> _objectDefinitionERC =
+		new CentralizedThreadLocal<>(
+			ObjectDefinitionThreadLocal.class + "._objectDefinitionERC");
+	private static final CentralizedThreadLocal<List<ObjectField>>
+		_objectDefinitionObjectFields = new CentralizedThreadLocal<>(
+			ObjectDefinitionThreadLocal.class +
+				"._objectDefinitionObjectFields");
+	private static final CentralizedThreadLocal<Boolean> _skipThrowException =
+		new CentralizedThreadLocal<>(
+			ObjectDefinitionThreadLocal.class + "._skipThrowException",
+			() -> Boolean.FALSE);
+	private static final CentralizedThreadLocal
+		<List<ObjectEntryValidationException.ValidationError>>
+			_validationErrors = new CentralizedThreadLocal<>(
+				ObjectDefinitionThreadLocal.class + "._validationErrors");
 
 }
