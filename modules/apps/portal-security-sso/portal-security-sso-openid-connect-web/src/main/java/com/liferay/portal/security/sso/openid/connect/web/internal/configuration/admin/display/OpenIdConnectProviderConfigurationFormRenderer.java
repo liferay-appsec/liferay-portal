41789fc640133e5212f9041497fbb4353f36c4e2
Rafael Praxedes <rafael.praxedes@liferay.com>
2025-09-17 17:02:07 -0300

    LPD-55735 Move to the right package

--- modules/apps/portal-security-sso/portal-security-sso-openid-connect-web/src/main/java/com/liferay/portal/security/sso/openid/connect/web/internal/configuration/admin/display/OpenIdConnectProviderConfigurationFormRenderer.java
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-web/src/main/java/com/liferay/portal/security/sso/openid/connect/web/internal/configuration/admin/display/OpenIdConnectProviderConfigurationFormRenderer.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.security.sso.openid.connect.web.internal;
+package com.liferay.portal.security.sso.openid.connect.web.internal.configuration.admin.display;
 
 import com.liferay.configuration.admin.display.ConfigurationFormRenderer;
 import com.liferay.expando.kernel.service.ExpandoColumnLocalService;
