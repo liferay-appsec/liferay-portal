c1c4f0fff6a8c33da52f74756d5035fa4c1cbfaa
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2025-12-02 12:43:59 -0300

    LPD-68730 Apply the API instead of using document.cookie directly

--- modules/apps/site-initializer/site-initializer-teaser-showcase/src/main/resources/site-initializer/fragments/group/tracking/fragments/interest/index.js
+++ modules/apps/site-initializer/site-initializer-teaser-showcase/src/main/resources/site-initializer/fragments/group/tracking/fragments/interest/index.js
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {COOKIE_TYPES, setCookie as setCookieUtil} from 'frontend-js-web';
+
 function setCookie(name, value, expirationDays) {
 	const expirationDate = new Date();
 
@@ -10,9 +12,11 @@ function setCookie(name, value, expirationDays) {
 		expirationDate.getTime() + expirationDays * 24 * 60 * 60 * 1000
 	);
 
-	const expires = 'expires=' + expirationDate.toUTCString();
-
-	document.cookie = `${name}=${value};${expires};path=/;secure`;
+	setCookieUtil(name, value, COOKIE_TYPES.FUNCTIONAL, {
+		'max-age': expirationDate,
+		'path': themeDisplay.getPathContext() || '/',
+		'secure': true
+	});
 }
 
 setCookie('Interest', 'Products', 1);
