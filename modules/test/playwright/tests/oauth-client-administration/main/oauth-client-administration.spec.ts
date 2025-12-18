2b464214fdcf760da9a98e9ce0a2f0d266b538b4
ChrisKian <christopher.kian@liferay.com>
2025-12-18 15:31:21 -0800

    LPD-67473 Include LPD in the tag attribute and auto SF

--- modules/test/playwright/tests/oauth-client-administration/main/oauth-client-administration.spec.ts
+++ modules/test/playwright/tests/oauth-client-administration/main/oauth-client-administration.spec.ts
@@ -16,37 +16,40 @@ const test = mergeTests(
 	loginTest()
 );
 
-test.describe('LPD-67473 Enable Configuration of oauth-authorization-server Well-Known URIs in the OAuthClient Portlet', () => {
-	test('Creation a oauth-authorization-server and validations', async ({
-		page,
-	}) => {
-		const authServerLocalMetadatasPage = new AuthServerLocalMetadatasPage(
-			page
-		);
+test.describe('Enable Configuration of oauth-authorization-server Well-Known URIs in the OAuthClient Portlet', () => {
+	test(
+		'Creation a oauth-authorization-server and validations',
+		{
+			tag: '@LPD-67473',
+		},
+		async ({page}) => {
+			const authServerLocalMetadatasPage =
+				new AuthServerLocalMetadatasPage(page);
 
-		await authServerLocalMetadatasPage.goTo();
+			await authServerLocalMetadatasPage.goTo();
 
-		await authServerLocalMetadatasPage.deleteAuthServerLocalMetadata();
+			await authServerLocalMetadatasPage.deleteAuthServerLocalMetadata();
 
-		await authServerLocalMetadatasPage.addAuthServerLocalMetadata(
-			'',
-			'The Issuer field is required.'
-		);
+			await authServerLocalMetadatasPage.addAuthServerLocalMetadata(
+				'',
+				'The Issuer field is required.'
+			);
 
-		await authServerLocalMetadatasPage.addAuthServerLocalMetadata(
-			'https://localhost.com'
-		);
+			await authServerLocalMetadatasPage.addAuthServerLocalMetadata(
+				'https://localhost.com'
+			);
 
-		await authServerLocalMetadatasPage.addAuthServerLocalMetadata(
-			'https://localhost.com',
-			'Duplicate'
-		);
+			await authServerLocalMetadatasPage.addAuthServerLocalMetadata(
+				'https://localhost.com',
+				'Duplicate'
+			);
 
-		await authServerLocalMetadatasPage.checkResult(
-			'https://localhost.com/o/.well-known/oauth-authorization-server',
-			'https://localhost.com/.well-known/openid-configuration/1B2M2Y8AsgTpgAmY7PhCfg**/local'
-		);
+			await authServerLocalMetadatasPage.checkResult(
+				'https://localhost.com/o/.well-known/oauth-authorization-server',
+				'https://localhost.com/.well-known/openid-configuration/1B2M2Y8AsgTpgAmY7PhCfg**/local'
+			);
 
-		await authServerLocalMetadatasPage.deleteAuthServerLocalMetadata();
-	});
+			await authServerLocalMetadatasPage.deleteAuthServerLocalMetadata();
+		}
+	);
 });
