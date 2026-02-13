3d85c6598ed128c13a3efc3be3b1adaf0342e7e0
ChrisKian <christopher.kian@liferay.com>
2026-02-12 16:20:58 -0800

    LPD-78638 Refactor cookie related playwright code to make it dryer and prevent flakes

---
+++ modules/test/playwright/tests/cookies-banner-web/main/utils/consentManagerConfigurationHelper.ts
@@ -0,0 +1,182 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {SystemSettingsPage} from '../../../../pages/configuration-admin-web/SystemSettingsPage';
+import {clickAndExpectToBeVisible} from '../../../../utils/clickAndExpectToBeVisible';
+import {reloadUntilVisible} from '../../../../utils/reloadUntilVisible';
+import {waitForAlert} from '../../../../utils/waitForAlert';
+
+interface ConsentManagerConfiguration {
+	consentRenewalPeriod?: string;
+	enabled?: boolean;
+	explicitCookieConsentMode?: boolean;
+	forceReload?: boolean;
+	storeConsent?: boolean;
+}
+
+export async function clearConsentCookies(systemSettingsPage) {
+	await systemSettingsPage.page
+		.context()
+		.clearCookies({name: /^CONSENT_TYPE_/});
+	await systemSettingsPage.page
+		.context()
+		.clearCookies({name: /^USER_CONSENT_CONFIGURED/});
+}
+
+export async function resetAllConsentManagerConfigurations(systemSettingsPage) {
+	await systemSettingsPage.goToSystemSetting('Privacy', 'Consent Manager');
+
+	const menuItems = await systemSettingsPage.page.getByRole('menuitem').all();
+
+	for (const menuItem of menuItems.reverse()) {
+		await menuItem.click();
+
+		await systemSettingsPage.page.waitForTimeout(1000);
+
+		await systemSettingsPage.page.waitForLoadState();
+
+		let dialog = false;
+
+		if (await menuItem.getByText('Product Analytics').isVisible()) {
+			continue;
+		}
+		else if (await menuItem.getByText('Consent Manager').isVisible()) {
+			dialog = true;
+		}
+
+		await resetConfiguration(dialog, systemSettingsPage);
+	}
+}
+
+async function resetConfiguration(dialog = false, systemSettingsPage) {
+	if (
+		await systemSettingsPage.page
+			.getByRole('button', {name: 'Actions'})
+			.isVisible()
+	) {
+		if (dialog) {
+			systemSettingsPage.page.once('dialog', async (dialogWindow) => {
+				await dialogWindow.accept();
+			});
+		}
+
+		await clickAndExpectToBeVisible({
+			autoClick: true,
+			target: systemSettingsPage.page.getByRole('menuitem', {
+				name: 'Reset Default Values',
+			}),
+			trigger: systemSettingsPage.page.getByRole('button', {
+				name: 'Actions',
+			}),
+		});
+	}
+}
+
+export async function resetConsentManagerConfiguration(systemSettingsPage) {
+	await systemSettingsPage.goToSystemSetting('Privacy', 'Consent Manager');
+
+	await systemSettingsPage.page.waitForTimeout(1000);
+
+	await systemSettingsPage.page.waitForLoadState();
+
+	await resetConfiguration(true, systemSettingsPage);
+}
+
+export async function updateConsentManagerConfiguration(
+	page,
+	{
+		consentRenewalPeriod,
+		enabled,
+		explicitCookieConsentMode,
+		forceReload,
+		storeConsent,
+	}: ConsentManagerConfiguration
+) {
+	if (forceReload) {
+		const systemSettingsPage = new SystemSettingsPage(page);
+		await systemSettingsPage.goToSystemSetting(
+			'Privacy',
+			'Consent Manager'
+		);
+	}
+
+	let dialog = false;
+
+	const enabledCheckbox = page.getByLabel('Enabled');
+
+	await enabledCheckbox.waitFor({state: 'visible'});
+
+	if (await enabledCheckbox.isChecked()) {
+		dialog = true;
+	}
+
+	if (enabled === true) {
+		await enabledCheckbox.setChecked(true);
+
+		await page.waitForTimeout(500);
+
+		if (explicitCookieConsentMode) {
+			await page
+				.getByLabel('Explicit Cookie Consent Mode')
+				.setChecked(explicitCookieConsentMode);
+		}
+
+		if (consentRenewalPeriod) {
+			await page
+				.getByLabel('Consent Renewal Period')
+				.fill(consentRenewalPeriod);
+		}
+
+		if (storeConsent) {
+			await page.getByLabel('Store Consent').setChecked(storeConsent);
+		}
+	}
+	else if (enabled === false) {
+		dialog = false;
+
+		await enabledCheckbox.setChecked(false);
+	}
+
+	await saveOrUpdateConfiguration(dialog, page);
+}
+
+async function saveOrUpdateConfiguration(dialog: boolean, page) {
+	if (dialog) {
+		page.once('dialog', async (dialogWindow) => {
+			await dialogWindow.accept();
+		});
+	}
+
+	const saveButton = page.getByRole('button', {
+		name: 'Save',
+	});
+
+	const updateButton = page.getByRole('button', {
+		name: 'Update',
+	});
+
+	if (await saveButton.isVisible()) {
+		await saveButton.dispatchEvent('click');
+	}
+	else if (await updateButton.isVisible()) {
+		await updateButton.dispatchEvent('click');
+	}
+
+	try {
+		await waitForAlert(page, undefined, {timeout: 5000});
+	}
+	catch (error) {
+		if (page.getByRole('heading', {name: '500'})) {
+			await reloadUntilVisible({
+				maxAttempts: 1,
+				myLocator: updateButton,
+				page,
+			});
+		}
+		else {
+			throw error;
+		}
+	}
+}
