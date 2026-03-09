e31a7c823f1e12f6b120563f451ebe06c80ea393
ChrisKian <christopher.kian@liferay.com>
2026-03-09 10:46:51 -0700

    LPD-76011 buildRest

---
+++ modules/apps/cookies/cookies-rest-client/src/main/java/com/liferay/cookies/rest/client/dto/v1_0/ConsentPreference.java
@@ -0,0 +1,182 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.cookies.rest.client.dto.v1_0;
+
+import com.liferay.cookies.rest.client.function.UnsafeSupplier;
+import com.liferay.cookies.rest.client.serdes.v1_0.ConsentPreferenceSerDes;
+
+import jakarta.annotation.Generated;
+
+import java.io.Serializable;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * @author Christopher Kian
+ * @generated
+ */
+@Generated("")
+public class ConsentPreference implements Cloneable, Serializable {
+
+	public static ConsentPreference toDTO(String json) {
+		return ConsentPreferenceSerDes.toDTO(json);
+	}
+
+	public String getDomain() {
+		return domain;
+	}
+
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+
+	public void setDomain(
+		UnsafeSupplier<String, Exception> domainUnsafeSupplier) {
+
+		try {
+			domain = domainUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected String domain;
+
+	public Date getExpirationDate() {
+		return expirationDate;
+	}
+
+	public void setExpirationDate(Date expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+
+	public void setExpirationDate(
+		UnsafeSupplier<Date, Exception> expirationDateUnsafeSupplier) {
+
+		try {
+			expirationDate = expirationDateUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected Date expirationDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setId(UnsafeSupplier<Long, Exception> idUnsafeSupplier) {
+		try {
+			id = idUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected Long id;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setName(UnsafeSupplier<String, Exception> nameUnsafeSupplier) {
+		try {
+			name = nameUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected String name;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public void setUserId(
+		UnsafeSupplier<Long, Exception> userIdUnsafeSupplier) {
+
+		try {
+			userId = userIdUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected Long userId;
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public void setValue(
+		UnsafeSupplier<String, Exception> valueUnsafeSupplier) {
+
+		try {
+			value = valueUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected String value;
+
+	@Override
+	public ConsentPreference clone() throws CloneNotSupportedException {
+		return (ConsentPreference)super.clone();
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+
+		if (!(object instanceof ConsentPreference)) {
+			return false;
+		}
+
+		ConsentPreference consentPreference = (ConsentPreference)object;
+
+		return Objects.equals(toString(), consentPreference.toString());
+	}
+
+	@Override
+	public int hashCode() {
+		String string = toString();
+
+		return string.hashCode();
+	}
+
+	public String toString() {
+		return ConsentPreferenceSerDes.toJSON(this);
+	}
+
+}
---
+++ modules/apps/cookies/cookies-rest-impl/src/main/java/com/liferay/cookies/rest/internal/graphql/query/v1_0/Query.java
@@ -0,0 +1,174 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.cookies.rest.internal.graphql.query.v1_0;
+
+import com.liferay.cookies.rest.dto.v1_0.ConsentPreference;
+import com.liferay.cookies.rest.resource.v1_0.ConsentPreferenceResource;
+import com.liferay.petra.function.UnsafeConsumer;
+import com.liferay.petra.function.UnsafeFunction;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.ResourceActionLocalService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
+import com.liferay.portal.vulcan.graphql.annotation.GraphQLField;
+import com.liferay.portal.vulcan.graphql.annotation.GraphQLName;
+import com.liferay.portal.vulcan.pagination.Page;
+
+import jakarta.annotation.Generated;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import jakarta.ws.rs.core.UriInfo;
+
+import java.util.Map;
+import java.util.function.BiFunction;
+
+import org.osgi.service.component.ComponentServiceObjects;
+
+/**
+ * @author Christopher Kian
+ * @generated
+ */
+@Generated("")
+public class Query {
+
+	public static void setConsentPreferenceResourceComponentServiceObjects(
+		ComponentServiceObjects<ConsentPreferenceResource>
+			consentPreferenceResourceComponentServiceObjects) {
+
+		_consentPreferenceResourceComponentServiceObjects =
+			consentPreferenceResourceComponentServiceObjects;
+	}
+
+	/**
+	 * Invoke this method with the command line:
+	 *
+	 * curl -H 'Content-Type: text/plain; charset=utf-8' -X 'POST' 'http://localhost:8080/o/graphql' -d $'{"query": "query {consentPreferenceByName(name: ___){domain, expirationDate, id, name, userId, value}}"}' -u 'test@liferay.com:test'
+	 */
+	@GraphQLField(
+		description = "Retrieves the specified consent preference of the user who made the request."
+	)
+	public ConsentPreference consentPreferenceByName(
+			@GraphQLName("name") String name)
+		throws Exception {
+
+		return _applyComponentServiceObjects(
+			_consentPreferenceResourceComponentServiceObjects,
+			this::_populateResourceContext,
+			consentPreferenceResource ->
+				consentPreferenceResource.getConsentPreferenceByName(name));
+	}
+
+	/**
+	 * Invoke this method with the command line:
+	 *
+	 * curl -H 'Content-Type: text/plain; charset=utf-8' -X 'POST' 'http://localhost:8080/o/graphql' -d $'{"query": "query {consentPreferences{items {__}, page, pageSize, totalCount}}"}' -u 'test@liferay.com:test'
+	 */
+	@GraphQLField(
+		description = "Retrieves the consent preferences of the user who made the request."
+	)
+	public ConsentPreferencePage consentPreferences() throws Exception {
+		return _applyComponentServiceObjects(
+			_consentPreferenceResourceComponentServiceObjects,
+			this::_populateResourceContext,
+			consentPreferenceResource -> new ConsentPreferencePage(
+				consentPreferenceResource.getConsentPreferencesPage()));
+	}
+
+	@GraphQLName("ConsentPreferencePage")
+	public class ConsentPreferencePage {
+
+		public ConsentPreferencePage(Page consentPreferencePage) {
+			actions = consentPreferencePage.getActions();
+
+			items = consentPreferencePage.getItems();
+			lastPage = consentPreferencePage.getLastPage();
+			page = consentPreferencePage.getPage();
+			pageSize = consentPreferencePage.getPageSize();
+			totalCount = consentPreferencePage.getTotalCount();
+		}
+
+		@GraphQLField
+		protected Map<String, Map<String, String>> actions;
+
+		@GraphQLField
+		protected java.util.Collection<ConsentPreference> items;
+
+		@GraphQLField
+		protected long lastPage;
+
+		@GraphQLField
+		protected long page;
+
+		@GraphQLField
+		protected long pageSize;
+
+		@GraphQLField
+		protected long totalCount;
+
+	}
+
+	private <T, R, E1 extends Throwable, E2 extends Throwable> R
+			_applyComponentServiceObjects(
+				ComponentServiceObjects<T> componentServiceObjects,
+				UnsafeConsumer<T, E1> unsafeConsumer,
+				UnsafeFunction<T, R, E2> unsafeFunction)
+		throws E1, E2 {
+
+		T resource = componentServiceObjects.getService();
+
+		try {
+			unsafeConsumer.accept(resource);
+
+			return unsafeFunction.apply(resource);
+		}
+		finally {
+			componentServiceObjects.ungetService(resource);
+		}
+	}
+
+	private void _populateResourceContext(
+			ConsentPreferenceResource consentPreferenceResource)
+		throws Exception {
+
+		consentPreferenceResource.setContextAcceptLanguage(_acceptLanguage);
+		consentPreferenceResource.setContextCompany(_company);
+		consentPreferenceResource.setContextHttpServletRequest(
+			_httpServletRequest);
+		consentPreferenceResource.setContextHttpServletResponse(
+			_httpServletResponse);
+		consentPreferenceResource.setContextUriInfo(_uriInfo);
+		consentPreferenceResource.setContextUser(_user);
+		consentPreferenceResource.setGroupLocalService(_groupLocalService);
+		consentPreferenceResource.setResourceActionLocalService(
+			_resourceActionLocalService);
+		consentPreferenceResource.setResourcePermissionLocalService(
+			_resourcePermissionLocalService);
+		consentPreferenceResource.setRoleLocalService(_roleLocalService);
+	}
+
+	private static ComponentServiceObjects<ConsentPreferenceResource>
+		_consentPreferenceResourceComponentServiceObjects;
+
+	private AcceptLanguage _acceptLanguage;
+	private com.liferay.portal.kernel.model.Company _company;
+	private BiFunction
+		<Object, String, com.liferay.portal.kernel.search.filter.Filter>
+			_filterBiFunction;
+	private GroupLocalService _groupLocalService;
+	private HttpServletRequest _httpServletRequest;
+	private HttpServletResponse _httpServletResponse;
+	private ResourceActionLocalService _resourceActionLocalService;
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+	private RoleLocalService _roleLocalService;
+	private BiFunction<Object, String, com.liferay.portal.kernel.search.Sort[]>
+		_sortsBiFunction;
+	private UriInfo _uriInfo;
+	private com.liferay.portal.kernel.model.User _user;
+
+}
