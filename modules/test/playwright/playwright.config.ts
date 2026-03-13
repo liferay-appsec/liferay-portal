d5d024bd86cd968eaea542d39a1a637fe3a8d42b
Lucas Miranda <lucas.miranda@liferay.com>
2026-03-13 14:59:49 -0300

    LPD-81903 Add tests

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -132,6 +132,7 @@ import {config as portalSecurityContentSecurityPolicyConfig} from './tests/porta
 import {config as portalSecurityLdapConfig} from './tests/portal-security-ldap/main/config';
 import {config as portalSecurityScriptManagementWebConfig} from './tests/portal-security-script-management-web/main/config';
 import {config as portalSecurityServiceAccessPolicyService} from './tests/portal-security-service-access-policy-service/main/config';
+import {config as portalSettingsAuthenticationOpenSSOWebConfig} from './tests/portal-settings-authentication-opensso-web/main/config';
 import {config as portalToolsRestBuilderTestImpl} from './tests/portal-tools-rest-builder-test-impl/main/config';
 import {config as portalUserLocaleOptionsConfig} from './tests/portal-user-locale-options-web/main/config';
 import {config as portalWebCDNConfig} from './tests/portal-web/cdn/config';
@@ -334,6 +335,7 @@ export default defineConfig({
 		portalSecurityLdapConfig,
 		portalSecurityScriptManagementWebConfig,
 		portalSecurityServiceAccessPolicyService,
+		portalSettingsAuthenticationOpenSSOWebConfig,
 		portalToolsRestBuilderTestImpl,
 		portalUserLocaleOptionsConfig,
 		portalWebCDNConfig,
