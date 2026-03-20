c85c1792d1a3e10cc479ab4f5acddd41cba3e914
Lucas Miranda <lucas.miranda@liferay.com>
2026-03-20 15:50:03 -0300

    LPD-76011 Move to local service

--- modules/apps/cookies/cookies-service/src/main/java/com/liferay/cookies/service/impl/ConsentPreferenceLocalServiceImpl.java
+++ modules/apps/cookies/cookies-service/src/main/java/com/liferay/cookies/service/impl/ConsentPreferenceLocalServiceImpl.java
@@ -5,10 +5,18 @@
 
 package com.liferay.cookies.service.impl;
 
+import com.liferay.cookies.model.ConsentPreference;
 import com.liferay.cookies.service.base.ConsentPreferenceLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+
+import java.util.Date;
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -19,4 +27,57 @@ import org.osgi.service.component.annotations.Component;
 )
 public class ConsentPreferenceLocalServiceImpl
 	extends ConsentPreferenceLocalServiceBaseImpl {
+
+	public ConsentPreference addConsentPreference(
+			long userId, String domain, Date expirationDate, String name,
+			String value)
+		throws PortalException {
+
+		ConsentPreference consentPreference =
+			consentPreferencePersistence.create(
+				counterLocalService.increment(
+					ConsentPreference.class.getName()));
+
+		User user = _userLocalService.getUser(userId);
+
+		consentPreference.setUserId(userId);
+		consentPreference.setUserName(user.getFullName());
+
+		consentPreference.setDomain(domain);
+		consentPreference.setExpirationDate(expirationDate);
+		consentPreference.setName(name);
+		consentPreference.setValue(value);
+
+		return consentPreferencePersistence.update(consentPreference);
+	}
+
+	public void deleteConsentPreference(long userId, String domain, String name)
+		throws PortalException {
+
+		consentPreferencePersistence.removeByU_D_N(userId, domain, name);
+	}
+
+	public void deleteConsentPreferences(long userId) {
+		consentPreferencePersistence.removeByUserId(userId);
+	}
+
+	public void deleteConsentPreferences(long userId, String domain) {
+		consentPreferencePersistence.removeByU_D(userId, domain);
+	}
+
+	public ConsentPreference fetchConsentPreference(
+		long userId, String domain, String name) {
+
+		return consentPreferencePersistence.fetchByU_D_N(userId, domain, name);
+	}
+
+	public List<ConsentPreference> getConsentPreferences(
+		long userId, String domain) {
+
+		return consentPreferencePersistence.findByU_D(userId, domain);
+	}
+
+	@Reference
+	private UserLocalService _userLocalService;
+
 }
--- modules/apps/cookies/cookies-service/src/main/java/com/liferay/cookies/service/impl/ConsentPreferenceServiceImpl.java
+++ modules/apps/cookies/cookies-service/src/main/java/com/liferay/cookies/service/impl/ConsentPreferenceServiceImpl.java
@@ -5,18 +5,10 @@
 
 package com.liferay.cookies.service.impl;
 
-import com.liferay.cookies.model.ConsentPreference;
 import com.liferay.cookies.service.base.ConsentPreferenceServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.service.UserLocalService;
-
-import java.util.Date;
-import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -31,63 +23,4 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class ConsentPreferenceServiceImpl
 	extends ConsentPreferenceServiceBaseImpl {
-
-	public ConsentPreference addConsentPreference(
-			long userId, String domain, Date expirationDate, String name,
-			String value)
-		throws PortalException {
-
-		ConsentPreference consentPreference =
-			consentPreferencePersistence.create(
-				counterLocalService.increment(
-					ConsentPreference.class.getName()));
-
-		User user = _userLocalService.getUser(userId);
-
-		consentPreference.setUserId(userId);
-		consentPreference.setUserName(user.getFullName());
-
-		consentPreference.setDomain(domain);
-		consentPreference.setExpirationDate(expirationDate);
-		consentPreference.setName(name);
-		consentPreference.setValue(value);
-
-		return consentPreferencePersistence.update(consentPreference);
-	}
-
-	public void deleteConsentPreference(long userId, String domain, String name)
-		throws PortalException {
-
-		consentPreferencePersistence.removeByU_D_N(userId, domain, name);
-	}
-
-	public void deleteConsentPreferences(long userId) {
-		consentPreferencePersistence.removeByUserId(userId);
-	}
-
-	public void deleteConsentPreferences(long userId, String domain) {
-		consentPreferencePersistence.removeByU_D(userId, domain);
-	}
-
-	public ConsentPreference getConsentPreference(
-		long userId, String domain, String name) {
-
-		return consentPreferencePersistence.fetchByU_D_N(userId, domain, name);
-	}
-
-	public List<ConsentPreference> getConsentPreferences(
-		long userId, String domain) {
-
-		return consentPreferencePersistence.findByU_D(userId, domain);
-	}
-
-	public ConsentPreference updateConsentPreference(
-		ConsentPreference consentPreference) {
-
-		return consentPreferencePersistence.updateImpl(consentPreference);
-	}
-
-	@Reference
-	private UserLocalService _userLocalService;
-
 }
