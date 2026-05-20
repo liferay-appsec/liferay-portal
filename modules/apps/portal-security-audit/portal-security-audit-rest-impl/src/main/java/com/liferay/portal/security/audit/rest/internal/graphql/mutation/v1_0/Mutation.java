82938709571481305ca997e7be92032211caeeeb
Rafael Praxedes <rafael.praxedes@liferay.com>
2026-05-20 17:42:45 -0300

    LPD-91075 portal-security-audit-rest-* - buildREST output
    Autogenerated by gradlew buildREST from the hand-written rest-config.yaml and rest-openapi.yaml on portal-security-audit-rest-impl. Contains the DTO, resource interface, base resource impl, client SerDes, GraphQL stubs, and the test-case scaffold. The empty AuditEventResourceImpl will be wired up in a follow-up commit.

---
+++ modules/apps/portal-security-audit/portal-security-audit-rest-impl/src/main/java/com/liferay/portal/security/audit/rest/internal/graphql/mutation/v1_0/Mutation.java
@@ -0,0 +1,155 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.security.audit.rest.internal.graphql.mutation.v1_0;
+
+import com.liferay.petra.function.UnsafeConsumer;
+import com.liferay.petra.function.UnsafeFunction;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.security.audit.rest.resource.v1_0.AuditEventResource;
+import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
+import com.liferay.portal.vulcan.batch.engine.resource.VulcanBatchEngineExportTaskResource;
+import com.liferay.portal.vulcan.batch.engine.resource.VulcanBatchEngineImportTaskResource;
+import com.liferay.portal.vulcan.graphql.annotation.GraphQLField;
+import com.liferay.portal.vulcan.graphql.annotation.GraphQLName;
+
+import jakarta.annotation.Generated;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+
+import java.util.Date;
+import java.util.function.BiFunction;
+
+import org.osgi.service.component.ComponentServiceObjects;
+
+/**
+ * @author Rafael Praxedes
+ * @generated
+ */
+@Generated("")
+public class Mutation {
+
+	public static void setAuditEventResourceComponentServiceObjects(
+		ComponentServiceObjects<AuditEventResource>
+			auditEventResourceComponentServiceObjects) {
+
+		_auditEventResourceComponentServiceObjects =
+			auditEventResourceComponentServiceObjects;
+	}
+
+	@GraphQLField
+	public Response createAuditEventsPageExportBatch(
+			@GraphQLName("accountEntryId") Long accountEntryId,
+			@GraphQLName("endDate") Date endDate,
+			@GraphQLName("entityId") Long entityId,
+			@GraphQLName("entityType") String entityType,
+			@GraphQLName("eventType") String eventType,
+			@GraphQLName("search") String search,
+			@GraphQLName("startDate") Date startDate,
+			@GraphQLName("userId") Long userId,
+			@GraphQLName("filter") String filterString,
+			@GraphQLName("sort") String sortsString,
+			@GraphQLName("callbackURL") String callbackURL,
+			@GraphQLName("contentType") String contentType,
+			@GraphQLName("fieldNames") String fieldNames)
+		throws Exception {
+
+		return _applyComponentServiceObjects(
+			_auditEventResourceComponentServiceObjects,
+			this::_populateResourceContext,
+			auditEventResource ->
+				auditEventResource.postAuditEventsPageExportBatch(
+					accountEntryId, endDate, entityId, entityType, eventType,
+					search, startDate, userId,
+					_filterBiFunction.apply(auditEventResource, filterString),
+					_sortsBiFunction.apply(auditEventResource, sortsString),
+					callbackURL, contentType, fieldNames));
+	}
+
+	private <T, R, E1 extends Throwable, E2 extends Throwable> R
+			_applyComponentServiceObjects(
+				ComponentServiceObjects<T> componentServiceObjects,
+				UnsafeConsumer<T, E1> unsafeConsumer,
+				UnsafeFunction<T, R, E2> unsafeFunction)
+		throws E1, E2 {
+
+		T resource = componentServiceObjects.getService();
+
+		try {
+			unsafeConsumer.accept(resource);
+
+			return unsafeFunction.apply(resource);
+		}
+		finally {
+			componentServiceObjects.ungetService(resource);
+		}
+	}
+
+	private <T, E1 extends Throwable, E2 extends Throwable> void
+			_applyVoidComponentServiceObjects(
+				ComponentServiceObjects<T> componentServiceObjects,
+				UnsafeConsumer<T, E1> unsafeConsumer,
+				UnsafeConsumer<T, E2> unsafeFunction)
+		throws E1, E2 {
+
+		T resource = componentServiceObjects.getService();
+
+		try {
+			unsafeConsumer.accept(resource);
+
+			unsafeFunction.accept(resource);
+		}
+		finally {
+			componentServiceObjects.ungetService(resource);
+		}
+	}
+
+	private void _populateResourceContext(AuditEventResource auditEventResource)
+		throws Exception {
+
+		auditEventResource.setContextAcceptLanguage(_acceptLanguage);
+		auditEventResource.setContextCompany(_company);
+		auditEventResource.setContextHttpServletRequest(_httpServletRequest);
+		auditEventResource.setContextHttpServletResponse(_httpServletResponse);
+		auditEventResource.setContextUriInfo(_uriInfo);
+		auditEventResource.setContextUser(_user);
+		auditEventResource.setGroupLocalService(_groupLocalService);
+		auditEventResource.setRoleLocalService(_roleLocalService);
+
+		auditEventResource.setVulcanBatchEngineExportTaskResource(
+			_vulcanBatchEngineExportTaskResource);
+
+		auditEventResource.setVulcanBatchEngineImportTaskResource(
+			_vulcanBatchEngineImportTaskResource);
+	}
+
+	private static ComponentServiceObjects<AuditEventResource>
+		_auditEventResourceComponentServiceObjects;
+
+	private AcceptLanguage _acceptLanguage;
+	private com.liferay.portal.kernel.model.Company _company;
+	private BiFunction
+		<Object, String, com.liferay.portal.kernel.search.filter.Filter>
+			_filterBiFunction;
+	private GroupLocalService _groupLocalService;
+	private HttpServletRequest _httpServletRequest;
+	private HttpServletResponse _httpServletResponse;
+	private RoleLocalService _roleLocalService;
+	private BiFunction<Object, String, com.liferay.portal.kernel.search.Sort[]>
+		_sortsBiFunction;
+	private UriInfo _uriInfo;
+	private com.liferay.portal.kernel.model.User _user;
+	private VulcanBatchEngineExportTaskResource
+		_vulcanBatchEngineExportTaskResource;
+	private VulcanBatchEngineImportTaskResource
+		_vulcanBatchEngineImportTaskResource;
+
+}
+// LIFERAY-REST-BUILDER-HASH:284983018
