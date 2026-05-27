5f07acc9f274ab376fc783e4d989c9a963c4d7dc
Christian Moura <christian.moura@liferay.com>
2026-05-27 10:30:09 -0300

    LPD-88765 Cover third-party CMP routing in CookiesConsentResolverProviderImplTest

--- modules/apps/cookies/cookies-test/src/testIntegration/java/com/liferay/cookies/internal/consent/test/CookiesConsentResolverProviderImplTest.java
+++ modules/apps/cookies/cookies-test/src/testIntegration/java/com/liferay/cookies/internal/consent/test/CookiesConsentResolverProviderImplTest.java
@@ -9,7 +9,13 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.consent.management.platform.integration.configuration.ConsentManagementPlatformConfiguration;
 import com.liferay.cookies.consent.CookiesConsentResolver;
 import com.liferay.cookies.consent.CookiesConsentResolverProvider;
+import com.liferay.layout.test.util.LayoutTestUtil;
 import com.liferay.portal.configuration.test.util.CompanyConfigurationTemporarySwapper;
+import com.liferay.portal.configuration.test.util.GroupConfigurationTemporarySwapper;
+import com.liferay.portal.kernel.cookies.constants.CookiesConstants;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -17,6 +23,13 @@ import com.liferay.portal.test.rule.FeatureFlag;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
+import jakarta.servlet.http.Cookie;
+
+import java.net.URLEncoder;
+
+import java.nio.charset.StandardCharsets;
+
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -37,6 +50,13 @@ public class CookiesConsentResolverProviderImplTest {
 	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
 		new LiferayIntegrationTestRule();
 
+	@After
+	public void tearDown() throws Exception {
+		if (_group != null) {
+			GroupTestUtil.deleteGroup(_group);
+		}
+	}
+
 	@Test
 	public void testGetCookiesConsentResolver() throws Exception {
 		Assert.assertSame(
@@ -46,19 +66,66 @@ public class CookiesConsentResolverProviderImplTest {
 		try (CompanyConfigurationTemporarySwapper
 				companyConfigurationTemporarySwapper = _swap(false)) {
 
+			MockHttpServletRequest mockHttpServletRequest =
+				_createMockHttpServletRequest();
+
 			Assert.assertSame(
 				_defaultCookiesConsentResolver,
 				_cookiesConsentResolverProvider.getCookiesConsentResolver(
-					_createMockHttpServletRequest()));
+					mockHttpServletRequest));
+
+			mockHttpServletRequest.setCookies(
+				new Cookie(
+					CookiesConstants.NAME_CONSENT_TYPE_FUNCTIONAL, "true"),
+				new Cookie(
+					CookiesConstants.NAME_LIFERAY_CONSENT_STATE,
+					URLEncoder.encode(
+						"{\"functional\":false}", StandardCharsets.UTF_8)));
+
+			CookiesConsentResolver cookiesConsentResolver =
+				_cookiesConsentResolverProvider.getCookiesConsentResolver(
+					mockHttpServletRequest);
+
+			Assert.assertTrue(
+				cookiesConsentResolver.hasConsent(
+					CookiesConstants.CONSENT_TYPE_FUNCTIONAL,
+					mockHttpServletRequest));
 		}
 
 		try (CompanyConfigurationTemporarySwapper
 				companyConfigurationTemporarySwapper = _swap(true)) {
 
+			MockHttpServletRequest mockHttpServletRequest =
+				_createMockHttpServletRequest();
+
 			Assert.assertSame(
-				_defaultCookiesConsentResolver,
+				_thirdPartyCookiesConsentResolver,
 				_cookiesConsentResolverProvider.getCookiesConsentResolver(
-					_createMockHttpServletRequest()));
+					mockHttpServletRequest));
+		}
+
+		_group = GroupTestUtil.addGroup();
+
+		_layout = LayoutTestUtil.addTypePortletLayout(_group);
+
+		MockHttpServletRequest mockHttpServletRequest =
+			_createMockHttpServletRequest();
+
+		mockHttpServletRequest.setAttribute(WebKeys.LAYOUT, _layout);
+
+		try (GroupConfigurationTemporarySwapper
+				groupConfigurationTemporarySwapper =
+					new GroupConfigurationTemporarySwapper(
+						_group.getGroupId(),
+						ConsentManagementPlatformConfiguration.class.getName(),
+						HashMapDictionaryBuilder.<String, Object>put(
+							"enabled", true
+						).build())) {
+
+			Assert.assertSame(
+				_thirdPartyCookiesConsentResolver,
+				_cookiesConsentResolverProvider.getCookiesConsentResolver(
+					mockHttpServletRequest));
 		}
 	}
 
@@ -92,5 +159,13 @@ public class CookiesConsentResolverProviderImplTest {
 		filter = "component.name=com.liferay.cookies.internal.consent.DefaultCookiesConsentResolver"
 	)
 	private CookiesConsentResolver _defaultCookiesConsentResolver;
+
+	private Group _group;
+	private Layout _layout;
+
+	@Inject(
+		filter = "component.name=com.liferay.consent.management.platform.integration.internal.consent.ThirdPartyCookiesConsentResolver"
+	)
+	private CookiesConsentResolver _thirdPartyCookiesConsentResolver;
 
 }
