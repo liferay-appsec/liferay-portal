f33ccf4f1af2fb8491a80cfb7346db12999fb30b
Christian Moura <christian.moura@liferay.com>
2026-05-27 13:47:12 -0300

    LPD-90487 Add tokenConnectionTimeout upgrade step

--- modules/apps/oauth-client/oauth-client-persistence-service/src/main/java/com/liferay/oauth/client/persistence/internal/upgrade/registry/OAuthClientPersistenceServiceUpgradeStepRegistrator.java
+++ modules/apps/oauth-client/oauth-client-persistence-service/src/main/java/com/liferay/oauth/client/persistence/internal/upgrade/registry/OAuthClientPersistenceServiceUpgradeStepRegistrator.java
@@ -8,6 +8,7 @@ package com.liferay.oauth.client.persistence.internal.upgrade.registry;
 import com.liferay.oauth.client.persistence.internal.upgrade.v1_4_0.OAuthClientASLocalMetadataUpgradeProcess;
 import com.liferay.oauth.client.persistence.internal.upgrade.v1_4_1.OAuthClientEntryUpgradeProcess;
 import com.liferay.oauth.client.persistence.internal.upgrade.v1_5_2.OAuthClientASLocalMetadataIssuerUpgradeProcess;
+import com.liferay.oauth.client.persistence.internal.upgrade.v1_5_4.OAuthClientEntryTokenConnectionTimeoutUpgradeProcess;
 import com.liferay.portal.kernel.upgrade.BaseExternalReferenceCodeUpgradeProcess;
 import com.liferay.portal.kernel.upgrade.BaseUuidUpgradeProcess;
 import com.liferay.portal.kernel.upgrade.UpgradeProcessFactory;
@@ -84,6 +85,11 @@ public class OAuthClientPersistenceServiceUpgradeStepRegistrator
 			"1.5.2", "1.5.3",
 			new com.liferay.oauth.client.persistence.internal.upgrade.v1_5_3.
 				OAuthClientASLocalMetadataUpgradeProcess());
+
+		registry.register(
+			"1.5.3", "1.5.4",
+			new OAuthClientEntryTokenConnectionTimeoutUpgradeProcess(
+				_configurationAdmin));
 	}
 
 	@Reference
