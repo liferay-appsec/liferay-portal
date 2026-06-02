720e70447bff4a57ce5a4adf7d2d999f07e8b74c
Christian Moura <christian.moura@liferay.com>
2026-06-02 10:39:16 -0300

    LPD-90487 Use local provider path in tokenConnectionTimeout integration tests

--- modules/apps/portal-security-sso/portal-security-sso-openid-connect-test/src/testIntegration/java/com/liferay/portal/security/sso/openid/connect/internal/configuration/instance/lifecycle/test/OpenIdConnectProviderPortalInstanceLifecycleListenerTest.java
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-test/src/testIntegration/java/com/liferay/portal/security/sso/openid/connect/internal/configuration/instance/lifecycle/test/OpenIdConnectProviderPortalInstanceLifecycleListenerTest.java
@@ -9,13 +9,19 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.oauth.client.persistence.constants.OAuthClientEntryConstants;
 import com.liferay.oauth.client.persistence.model.OAuthClientEntry;
 import com.liferay.oauth.client.persistence.service.OAuthClientEntryLocalService;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.configuration.test.util.ConfigurationTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.Base64;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.net.URI;
+
+import java.security.MessageDigest;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -47,53 +53,95 @@ public class OpenIdConnectProviderPortalInstanceLifecycleListenerTest {
 	}
 
 	@Test
-	public void testTokenConnectionTimeout() throws Exception {
+	public void testGetTokenConnectionTimeout() throws Exception {
 		String clientId1 = RandomTestUtil.randomString();
-		String discoveryEndpoint =
-			"https://accounts.google.com/.well-known/openid-configuration";
+		String issuerURL1 = "http://" + RandomTestUtil.randomString();
 		int tokenConnectionTimeout = RandomTestUtil.randomInt();
+		String tokenEndpoint1 = "http://" + RandomTestUtil.randomString();
 
 		_pid1 = ConfigurationTestUtil.createFactoryConfiguration(
 			"com.liferay.portal.security.sso.openid.connect.internal." +
 				"configuration.OpenIdConnectProviderConfiguration",
 			HashMapDictionaryBuilder.<String, Object>put(
+				"authorizationEndpoint",
+				"http://" + RandomTestUtil.randomString()
+			).put(
 				"companyId", TestPropsValues.getCompanyId()
 			).put(
-				"discoveryEndpoint", discoveryEndpoint
+				"issuerURL", issuerURL1
+			).put(
+				"jwksURI", "http://" + RandomTestUtil.randomString()
 			).put(
 				"openIdConnectClientId", clientId1
 			).put(
+				"subjectTypes", new String[] {"public"}
+			).put(
 				"tokenConnectionTimeout", tokenConnectionTimeout
+			).put(
+				"tokenEndpoint", tokenEndpoint1
+			).put(
+				"userInfoEndpoint", "http://" + RandomTestUtil.randomString()
 			).build());
 
 		OAuthClientEntry oAuthClientEntry1 =
 			_oAuthClientEntryLocalService.fetchOAuthClientEntry(
-				TestPropsValues.getCompanyId(), discoveryEndpoint, clientId1);
+				TestPropsValues.getCompanyId(),
+				_generateLocalWellKnownURI(issuerURL1, tokenEndpoint1),
+				clientId1);
 
 		Assert.assertEquals(
 			tokenConnectionTimeout,
 			oAuthClientEntry1.getTokenConnectionTimeout());
 
 		String clientId2 = RandomTestUtil.randomString();
+		String issuerURL2 = "http://" + RandomTestUtil.randomString();
+		String tokenEndpoint2 = "http://" + RandomTestUtil.randomString();
 
 		_pid2 = ConfigurationTestUtil.createFactoryConfiguration(
 			"com.liferay.portal.security.sso.openid.connect.internal." +
 				"configuration.OpenIdConnectProviderConfiguration",
 			HashMapDictionaryBuilder.<String, Object>put(
+				"authorizationEndpoint",
+				"http://" + RandomTestUtil.randomString()
+			).put(
 				"companyId", TestPropsValues.getCompanyId()
 			).put(
-				"discoveryEndpoint", discoveryEndpoint
+				"issuerURL", issuerURL2
+			).put(
+				"jwksURI", "http://" + RandomTestUtil.randomString()
 			).put(
 				"openIdConnectClientId", clientId2
+			).put(
+				"subjectTypes", new String[] {"public"}
+			).put(
+				"tokenEndpoint", tokenEndpoint2
+			).put(
+				"userInfoEndpoint", "http://" + RandomTestUtil.randomString()
 			).build());
 
 		OAuthClientEntry oAuthClientEntry2 =
 			_oAuthClientEntryLocalService.fetchOAuthClientEntry(
-				TestPropsValues.getCompanyId(), discoveryEndpoint, clientId2);
+				TestPropsValues.getCompanyId(),
+				_generateLocalWellKnownURI(issuerURL2, tokenEndpoint2),
+				clientId2);
 
 		Assert.assertEquals(
 			OAuthClientEntryConstants.TOKEN_CONNECTION_TIMEOUT_DEFAULT,
 			oAuthClientEntry2.getTokenConnectionTimeout());
+	}
+
+	private String _generateLocalWellKnownURI(
+			String issuerURL, String tokenEndpoint)
+		throws Exception {
+
+		MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+		URI issuerURI = URI.create(issuerURL);
+
+		return StringBundler.concat(
+			issuerURI.getScheme(), "://", issuerURI.getAuthority(),
+			"/.well-known/openid-configuration", issuerURI.getPath(), '/',
+			Base64.encodeToURL(messageDigest.digest(tokenEndpoint.getBytes())),
+			"/local");
 	}
 
 	@Inject
