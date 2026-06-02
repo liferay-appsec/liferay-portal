fc037a394c7e685e1638445576c5ee95c1a2ce0e
Christian Moura <christian.moura@liferay.com>
2026-06-02 10:38:22 -0300

    LPD-90487 Move OpenIdConnectProviderPortalInstanceLifecycleListenerTest to portal-security-sso-openid-connect-test

--- modules/apps/portal-security-sso/portal-security-sso-openid-connect-test/src/testIntegration/java/com/liferay/portal/security/sso/openid/connect/internal/configuration/instance/lifecycle/test/OpenIdConnectProviderPortalInstanceLifecycleListenerTest.java
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-test/src/testIntegration/java/com/liferay/portal/security/sso/openid/connect/internal/configuration/instance/lifecycle/test/OpenIdConnectProviderPortalInstanceLifecycleListenerTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.oauth.client.persistence.internal.configuration.instance.lifecycle.test;
+package com.liferay.portal.security.sso.openid.connect.internal.configuration.instance.lifecycle.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.oauth.client.persistence.constants.OAuthClientEntryConstants;
