2a95f6b588ffe9af9fb406ae5fa2b77faab23426
Lucas Miranda <lucas.miranda@liferay.com>
2026-06-11 15:11:13 -0300

    LPD-90288 Add FIPSModeUtil to portal-kernel
    Mirror the LDAP module's FIPSModeUtil in portal-kernel so portal core classes that select cryptographic algorithms (for example TunnelAuthenticationManagerImpl, UpgradeCompany, and PwdAuthenticator) can check FIPS mode and approved algorithms without depending on an application module. The existing com.liferay.portal.security.ldap.FIPSModeUtil is left in place.

---
+++ portal-kernel/src/com/liferay/portal/kernel/security/fips/FIPSModeUtil.java
@@ -0,0 +1,39 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.kernel.security.fips;
+
+import com.liferay.portal.kernel.util.PropsValues;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+/**
+ * @author Lucas Miranda
+ */
+public class FIPSModeUtil {
+
+	public static boolean isApprovedPasswordAlgorithm(String algorithm) {
+		if (Validator.isNull(algorithm)) {
+			return false;
+		}
+
+		String normalizedString = StringUtil.toUpperCase(algorithm);
+
+		if (normalizedString.startsWith("PBKDF2") ||
+			normalizedString.equals("SHA-256") ||
+			normalizedString.equals("SHA-384") ||
+			normalizedString.equals("SHA-512")) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	public static boolean isEnabled() {
+		return PropsValues.FIPS_ENABLED;
+	}
+
+}
