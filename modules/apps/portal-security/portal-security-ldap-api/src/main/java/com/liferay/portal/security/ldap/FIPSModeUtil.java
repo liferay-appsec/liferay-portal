1551d1aca0d49219a02878149e49fa0e568b4e08
Caio Farias <caio.farias@liferay.com>
2026-06-16 17:36:49 -0300

    LPD-86301 Add FIPS mode detection utility

---
+++ modules/apps/portal-security/portal-security-ldap-api/src/main/java/com/liferay/portal/security/ldap/FIPSModeUtil.java
@@ -0,0 +1,57 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.security.ldap;
+
+import com.liferay.portal.kernel.util.PropsValues;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Set;
+
+/**
+ * @author Jorge García Jiménez
+ */
+public class FIPSModeUtil {
+
+	public static boolean isEnabled() {
+		return PropsValues.FIPS_ENABLED;
+	}
+
+	public static boolean isNotAllowedAlgorithm(String algorithm) {
+		if (!isEnabled()) {
+			return false;
+		}
+
+		if (Validator.isNull(algorithm)) {
+			return true;
+		}
+
+		for (String allowedAlgorithm : _allowedAlgorithms) {
+			if (StringUtil.startsWith(algorithm, allowedAlgorithm)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static boolean isNotSecureProtocol(String url) {
+		if (!isEnabled()) {
+			return false;
+		}
+
+		if (Validator.isNull(url)) {
+			return true;
+		}
+
+		return !StringUtil.startsWith(url, "ldaps://");
+	}
+
+	private static final Set<String> _allowedAlgorithms = Set.of(
+		"PBKDF2WithHmacSHA1", "PBKDF2WithHmacSHA256", "PBKDF2WithHmacSHA384",
+		"PBKDF2WithHmacSHA512", "SHA-256", "SHA-384", "SHA-512");
+
+}
