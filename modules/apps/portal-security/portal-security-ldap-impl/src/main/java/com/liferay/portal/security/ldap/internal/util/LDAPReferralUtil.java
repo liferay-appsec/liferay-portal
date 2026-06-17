53ec1b440603b6dde1c2864baccada7e630573c1
Lucas Miranda <lucas.miranda@liferay.com>
2026-06-17 11:32:21 -0300

    LPD-94237 Block unsafe object resolution from non-LDAP JNDI referrals
    For installs that need referrals, a configured follow is downgraded internally to throw and only referrals whose URL uses the ldap or ldaps scheme are followed; rmi, corba, and any other protocol are logged and skipped before the unsafe object resolution sink is reached. The trustURLCodebase properties are disabled defensively in the JNDI environment as well. Because JNDI surfaces the referral as an exception on the search operation itself, the search call is handled inside the referral catching block so legitimate ldap referrals keep working and the configuration screen does not error.

---
+++ modules/apps/portal-security/portal-security-ldap-impl/src/main/java/com/liferay/portal/security/ldap/internal/util/LDAPReferralUtil.java
@@ -0,0 +1,225 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.security.ldap.internal.util;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.security.ldap.constants.LDAPConstants;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.Queue;
+
+import javax.naming.Context;
+import javax.naming.Name;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.ReferralException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+
+/**
+ * @author Lucas Miranda
+ */
+public class LDAPReferralUtil {
+
+	public static void addSafeReferralEnvironmentProperties(
+		Map<? super String, ? super String> environment, String referral) {
+
+		environment.put(_TRUST_URL_CODEBASE_COSNAMING, "false");
+		environment.put(_TRUST_URL_CODEBASE_LDAP, "false");
+		environment.put(_TRUST_URL_CODEBASE_RMI, "false");
+
+		if (Objects.equals(referral, LDAPConstants.REFERRAL_FOLLOW)) {
+			environment.put(Context.REFERRAL, LDAPConstants.REFERRAL_THROWS);
+		}
+		else {
+			environment.put(Context.REFERRAL, referral);
+		}
+	}
+
+	public static NamingEnumeration<SearchResult> searchWithSafeReferrals(
+			DirContext dirContext, String filter, Object[] filterArguments,
+			Name name, SearchControls searchControls)
+		throws NamingException {
+
+		List<SearchResult> searchResults = new ArrayList<>();
+
+		Queue<DirContext> dirContexts = new ArrayDeque<>();
+
+		dirContexts.add(dirContext);
+
+		int referralCount = 0;
+
+		try {
+			while (!dirContexts.isEmpty()) {
+				DirContext currentDirContext = dirContexts.poll();
+
+				NamingEnumeration<SearchResult> enumeration = null;
+
+				try {
+					enumeration = currentDirContext.search(
+						name, filter, filterArguments, searchControls);
+
+					while (enumeration.hasMore()) {
+						searchResults.add(enumeration.next());
+					}
+				}
+				catch (ReferralException referralException) {
+					referralCount = _followSafeReferrals(
+						dirContexts, referralCount, referralException);
+				}
+				finally {
+					if (enumeration != null) {
+						enumeration.close();
+					}
+
+					if (currentDirContext != dirContext) {
+						currentDirContext.close();
+					}
+				}
+			}
+		}
+		finally {
+			for (DirContext remainingDirContext : dirContexts) {
+				if (remainingDirContext != dirContext) {
+					_close(remainingDirContext);
+				}
+			}
+		}
+
+		return new ListNamingEnumeration(searchResults);
+	}
+
+	private static void _close(DirContext dirContext) {
+		try {
+			dirContext.close();
+		}
+		catch (NamingException namingException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					"Unable to close directory context", namingException);
+			}
+		}
+	}
+
+	private static int _followSafeReferrals(
+			Queue<DirContext> dirContexts, int referralCount,
+			ReferralException referralException)
+		throws NamingException {
+
+		boolean hasReferral = true;
+
+		while (hasReferral) {
+			Object referralInfo = referralException.getReferralInfo();
+
+			if ((referralInfo instanceof String) &&
+				_isAllowedReferralURL((String)referralInfo)) {
+
+				if (referralCount < _MAX_REFERRAL_COUNT) {
+					referralCount++;
+
+					dirContexts.add(
+						(DirContext)referralException.getReferralContext());
+				}
+				else if (_log.isDebugEnabled()) {
+					_log.debug(
+						"Stopped following LDAP referrals after reaching the " +
+							"maximum count of " + _MAX_REFERRAL_COUNT);
+				}
+			}
+			else if (_log.isDebugEnabled()) {
+				_log.debug(
+					"Blocked an LDAP referral to a non-LDAP URL: " +
+						referralInfo);
+			}
+
+			hasReferral = referralException.skipReferral();
+		}
+
+		return referralCount;
+	}
+
+	private static boolean _isAllowedReferralURL(String url) {
+		if (Validator.isNull(url)) {
+			return false;
+		}
+
+		String normalizedURL = StringUtil.toLowerCase(StringUtil.trim(url));
+
+		for (String referralURL : normalizedURL.split("\\s+")) {
+			if (!referralURL.startsWith("ldap://") &&
+				!referralURL.startsWith("ldaps://")) {
+
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private static final int _MAX_REFERRAL_COUNT = 10;
+
+	private static final String _TRUST_URL_CODEBASE_COSNAMING =
+		"com.sun.jndi.cosnaming.object.trustURLCodebase";
+
+	private static final String _TRUST_URL_CODEBASE_LDAP =
+		"com.sun.jndi.ldap.object.trustURLCodebase";
+
+	private static final String _TRUST_URL_CODEBASE_RMI =
+		"com.sun.jndi.rmi.object.trustURLCodebase";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LDAPReferralUtil.class);
+
+	private static class ListNamingEnumeration
+		implements NamingEnumeration<SearchResult> {
+
+		public ListNamingEnumeration(List<SearchResult> searchResults) {
+			_iterator = searchResults.iterator();
+		}
+
+		@Override
+		public void close() {
+		}
+
+		@Override
+		public boolean hasMore() {
+			return _iterator.hasNext();
+		}
+
+		@Override
+		public boolean hasMoreElements() {
+			return _iterator.hasNext();
+		}
+
+		@Override
+		public SearchResult next() {
+			return _iterator.next();
+		}
+
+		@Override
+		public SearchResult nextElement() {
+			if (!_iterator.hasNext()) {
+				throw new NoSuchElementException();
+			}
+
+			return _iterator.next();
+		}
+
+		private final Iterator<SearchResult> _iterator;
+
+	}
+
+}
