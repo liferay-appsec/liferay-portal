428bee96d981444382957bf87fd091bb0c469801
Jorge García Jiménez <jorge.garcia@liferay.com>
2026-07-02 09:49:04 +0200

    LPD-91327 SF

--- modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/dynamic/registration/container/request/filter/DynamicRegistrationServiceContainerRequestFilter.java
+++ modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/dynamic/registration/container/request/filter/DynamicRegistrationServiceContainerRequestFilter.java
@@ -37,6 +37,7 @@ import com.liferay.portal.kernel.settings.CompanyServiceSettingsLocator;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.kernel.util.Validator;
 
 import jakarta.annotation.Priority;
@@ -58,7 +59,6 @@ import java.security.Principal;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.cxf.jaxrs.utils.ExceptionUtils;
 import org.apache.cxf.jaxrs.utils.JAXRSUtils;
@@ -239,8 +239,7 @@ public class DynamicRegistrationServiceContainerRequestFilter
 			throw ExceptionUtils.toNotAuthorizedException(null, null);
 		}
 
-		long currentTime = TimeUnit.SECONDS.convert(
-			System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+		long currentTime = System.currentTimeMillis() / Time.SECOND;
 		long expirationTime = GetterUtil.getLong(jwtToken.getClaim("exp"));
 
 		if ((expirationTime > 0) && (currentTime > expirationTime)) {
@@ -441,8 +440,7 @@ public class DynamicRegistrationServiceContainerRequestFilter
 
 			if (accessTokenExpirationDate != null) {
 				jwtClaims.setExpiryTime(
-					TimeUnit.MILLISECONDS.toSeconds(
-						accessTokenExpirationDate.getTime()));
+					accessTokenExpirationDate.getTime() / Time.SECOND);
 			}
 
 			return new JwtToken(jwtClaims);
