2800e52c92e36ac3b6cd4ba7eb07d75bd289f4b6
Lucas Miranda <lucas.miranda@liferay.com>
2026-08-04 16:17:27 -0300

    LPD-93272 Implement crypto health verification endpoint

--- modules/apps/portal-security/portal-security-fips-rest-impl/src/main/java/com/liferay/portal/security/fips/rest/internal/resource/v1_0/HealthVerificationResourceImpl.java
+++ modules/apps/portal-security/portal-security-fips-rest-impl/src/main/java/com/liferay/portal/security/fips/rest/internal/resource/v1_0/HealthVerificationResourceImpl.java
@@ -5,7 +5,19 @@
 
 package com.liferay.portal.security.fips.rest.internal.resource.v1_0;
 
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.fips.FIPSApplicationStateMachineUtil;
+import com.liferay.portal.kernel.security.fips.FIPSModeValidator;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.util.PropsValues;
+import com.liferay.portal.security.fips.rest.dto.v1_0.HealthVerification;
 import com.liferay.portal.security.fips.rest.resource.v1_0.HealthVerificationResource;
+
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
+
+import java.util.Date;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ServiceScope;
@@ -19,4 +31,55 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class HealthVerificationResourceImpl
 	extends BaseHealthVerificationResourceImpl {
+
+	@Override
+	public HealthVerification postHealthVerification() throws Exception {
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		if (!permissionChecker.isOmniadmin() &&
+			!permissionChecker.isCompanyAdmin()) {
+
+			throw new PrincipalException.MustHavePermission(
+				permissionChecker, "TRIGGER_HEALTH_VERIFICATION");
+		}
+
+		HealthVerification healthVerification = new HealthVerification();
+
+		healthVerification.setDate(() -> new Date());
+
+		if (!PropsValues.FIPS_ENABLED) {
+			healthVerification.setStatus(
+				() -> HealthVerification.Status.NOT_APPLICABLE);
+
+			throw new WebApplicationException(
+				Response.status(
+					Response.Status.CONFLICT
+				).entity(
+					healthVerification
+				).build());
+		}
+
+		try {
+			FIPSApplicationStateMachineUtil.selfTest(
+				FIPSModeValidator::validate);
+		}
+		catch (Exception exception) {
+			healthVerification.setProviderMessage(exception::getMessage);
+			healthVerification.setStatus(
+				() -> HealthVerification.Status.FAILED);
+
+			throw new WebApplicationException(
+				Response.status(
+					Response.Status.SERVICE_UNAVAILABLE
+				).entity(
+					healthVerification
+				).build());
+		}
+
+		healthVerification.setStatus(() -> HealthVerification.Status.HEALTHY);
+
+		return healthVerification;
+	}
+
 }
