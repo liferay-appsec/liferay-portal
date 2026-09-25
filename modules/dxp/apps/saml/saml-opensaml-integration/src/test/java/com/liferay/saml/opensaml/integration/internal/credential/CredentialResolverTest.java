19971a67b4b1c01ae5b08ce84bd8ce5f2b64bb49
Caio Farias <caio.farias@liferay.com>
2026-09-25 09:32:13 -0300

    LPD-105589 Resolve the SAML encryption credential

--- modules/dxp/apps/saml/saml-opensaml-integration/src/test/java/com/liferay/saml/opensaml/integration/internal/credential/CredentialResolverTest.java
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/test/java/com/liferay/saml/opensaml/integration/internal/credential/CredentialResolverTest.java
@@ -5,8 +5,17 @@
 
 package com.liferay.saml.opensaml.integration.internal.credential;
 
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.security.key.KeyReference;
+import com.liferay.portal.security.key.KeyReferenceUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 import com.liferay.saml.opensaml.integration.internal.BaseSamlTestCase;
+import com.liferay.saml.opensaml.integration.internal.util.KeyStoreUtil;
+
+import java.security.KeyStore;
+import java.security.PrivateKey;
 
 import net.shibboleth.utilities.java.support.resolver.CriteriaSet;
 
@@ -15,8 +24,12 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+import org.mockito.Mockito;
+
 import org.opensaml.core.criterion.EntityIdCriterion;
 import org.opensaml.security.credential.Credential;
+import org.opensaml.security.credential.UsageType;
+import org.opensaml.security.criteria.UsageCriterion;
 
 /**
  * @author Mika Koivisto
@@ -53,6 +66,52 @@ public class CredentialResolverTest extends BaseSamlTestCase {
 		_testResolveCredential(SP_ENTITY_ID);
 	}
 
+	@Test
+	public void testResolveSpEncryptionCredential() throws Exception {
+		prepareServiceProvider(SP_ENTITY_ID);
+
+		KeyStore keyStore = fileSystemKeyStoreManagerImpl.getKeyStore();
+
+		String keyStoreCredentialPassword =
+			samlProviderConfiguration.keyStoreCredentialPassword();
+
+		KeyStore.PrivateKeyEntry privateKeyEntry =
+			(KeyStore.PrivateKeyEntry)keyStore.getEntry(
+				SP_ENTITY_ID,
+				new KeyStore.PasswordProtection(
+					keyStoreCredentialPassword.toCharArray()));
+
+		String keyStoreEncryptionCredentialPassword =
+			RandomTestUtil.randomString();
+
+		keyStore.setEntry(
+			KeyStoreUtil.getAlias(SP_ENTITY_ID, UsageType.ENCRYPTION),
+			privateKeyEntry,
+			new KeyStore.PasswordProtection(
+				keyStoreEncryptionCredentialPassword.toCharArray()));
+
+		String keyReferenceString = KeyReferenceUtil.toKeyReferenceString(
+			new KeyReference(
+				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+				KeyReference.Type.SECRET));
+
+		Mockito.when(
+			secretResolver.resolve(COMPANY_ID, keyReferenceString)
+		).thenReturn(
+			keyStoreEncryptionCredentialPassword
+		);
+
+		try (SafeCloseable safeCloseable =
+				CompanyThreadLocal.setCompanyIdWithSafeCloseable(COMPANY_ID)) {
+
+			_testResolveSpEncryptionCredential(
+				keyReferenceString, privateKeyEntry.getPrivateKey());
+			_testResolveSpEncryptionCredential(
+				keyStoreEncryptionCredentialPassword,
+				privateKeyEntry.getPrivateKey());
+		}
+	}
+
 	private void _testResolveCredential(String spEntityId) throws Exception {
 		EntityIdCriterion entityIDCriterion = new EntityIdCriterion(spEntityId);
 
@@ -63,6 +122,25 @@ public class CredentialResolverTest extends BaseSamlTestCase {
 		Credential credential = credentialResolver.resolveSingle(criteriaSet);
 
 		Assert.assertNotNull(credential);
+	}
+
+	private void _testResolveSpEncryptionCredential(
+			String keyStoreEncryptionCredentialPassword, PrivateKey privateKey)
+		throws Exception {
+
+		Mockito.when(
+			samlProviderConfiguration.keyStoreEncryptionCredentialPassword()
+		).thenReturn(
+			keyStoreEncryptionCredentialPassword
+		);
+
+		Credential credential = credentialResolver.resolveSingle(
+			new CriteriaSet(
+				new EntityIdCriterion(SP_ENTITY_ID),
+				new UsageCriterion(UsageType.ENCRYPTION)));
+
+		Assert.assertEquals(privateKey, credential.getPrivateKey());
+		Assert.assertEquals(UsageType.ENCRYPTION, credential.getUsageType());
 	}
 
 }
